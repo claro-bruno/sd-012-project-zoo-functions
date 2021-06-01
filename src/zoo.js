bc4e4d4e7ca3ce1ee5667630c8d43dc6c2739308
Gabriel Takaki <gabitj@icloud.com>
2021-06-01 18:32:20 -0300

    Lint

--- src/zoo.js
+++ src/zoo.js
@@ -31,52 +31,51 @@ function getEmployeeByName(employeeName) {
   // seu código aqui
   if (employees.find((employee) => (
     employeeName === employee.firstName || employeeName === employee.lastName))) {
-    return employees.find((employee) => 
-    (employeeName === employee.firstName || employeeName === employee.lastName));
-  } else {
-    return {};
+    return employees.find((employee) =>
+      (employeeName === employee.firstName || employeeName === employee.lastName));
   }
+  return {};
 }
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+}
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  // seu código aqui
+}
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  // seu código aqui
+}
 
-// function countAnimals(species) {
-//   // seu código aqui
-// }
+function countAnimals(species) {
+  // seu código aqui
+}
 
-// function calculateEntry(entrants) {
-//   // seu código aqui
-// }
+function calculateEntry(entrants) {
+  // seu código aqui
+}
 
-// function getAnimalMap(options) {
-//   // seu código aqui
-// }
+function getAnimalMap(options) {
+  // seu código aqui
+}
 
-// function getSchedule(dayName) {
-//   // seu código aqui
-// }
+function getSchedule(dayName) {
+  // seu código aqui
+}
 
-// function getOldestFromFirstSpecies(id) {
-//   // seu código aqui
-// }
+function getOldestFromFirstSpecies(id) {
+  // seu código aqui
+}
 
-// function increasePrices(percentage) {
-//   // seu código aqui
-// }
+function increasePrices(percentage) {
+  // seu código aqui
+}
 
-// function getEmployeeCoverage(idOrName) {
-//   // seu código aqui
-// }
+function getEmployeeCoverage(idOrName) {
+  // seu código aqui
+}
 
 module.exports = {
   calculateEntry,
