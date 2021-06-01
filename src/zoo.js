ccc3e2bca1d665f25ecdf02f3c5ae2448851d578
alestori <alestori7@gmail.com>
2021-06-01 14:09:31 -0300

    requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -15,10 +15,10 @@ function getSpeciesByIds(...ids) {
   // seu código aqui
   return species.filter((specie) => ids.some((id) => specie.id === id));
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.find((name) => species.name === animal).residents.every((resident) => resident.age === age);
 }
 
 function getEmployeeByName(employeeName) {
