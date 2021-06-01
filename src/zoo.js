b4606bcf07c1d284e74b126e97b7b1bccab0680b
Gabriel Takaki <gabitj@icloud.com>
2021-06-01 18:26:21 -0300

    Lint

--- src/zoo.js
+++ src/zoo.js
@@ -15,8 +15,8 @@ const data = require('./data');
 function getSpeciesByIds(...ids) {
   // seu código aqui
   return data.species
-  .filter((animal) => ids
-  .find((animalId) => animalId === animal.id));
+    .filter((animal) => ids
+      .find((animalId) => animalId === animal.id));
 }
 
 function getAnimalsOlderThan(animal, age) {
@@ -29,54 +29,54 @@ function getAnimalsOlderThan(animal, age) {
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
-  if (employees.find((employee) => employeeName === employee.firstName || employeeName === employee.lastName)) {
-    return employees.find((employee) => employeeName === employee.firstName || employeeName === employee.lastName)
-  } else{
+  if (employees.find((employee) => (
+    employeeName === employee.firstName || employeeName === employee.lastName))) {
+    return employees.find((employee) => 
+    (employeeName === employee.firstName || employeeName === employee.lastName));
+  } else {
     return {};
   }
 }
 
-console.log(getEmployeeByName());
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function countAnimals(species) {
+//   // seu código aqui
+// }
 
-function countAnimals(species) {
-  // seu código aqui
-}
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+// function getSchedule(dayName) {
+//   // seu código aqui
+// }
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
-
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function getEmployeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
   calculateEntry,
