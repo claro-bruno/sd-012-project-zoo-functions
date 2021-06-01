9556776ffcbf8b6a183aa477f351c3b3a9321ca0
Roberval Filho <rslfilho@gmail.com>
2021-06-01 16:30:19 -0300

    Requisitos 08, 10 e 11

--- src/zoo.js
+++ src/zoo.js
@@ -70,21 +70,102 @@ const countAnimals = (species) => {
   return result;
 };
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const calculateEntry = (entrants) => {
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const { Adult: adults = 0, Child: childs = 0, Senior: seniors = 0 } = entrants;
+  const { Adult, Senior, Child } = data.prices;
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+  const result = adults * Adult + childs * Child + seniors * Senior;
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+  return result;
+};
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const locations = ['NE', 'NW', 'SE', 'SW'];
+
+const getLocation = () => {
+  const result = {};
+  locations.forEach((loc) => {
+    const animals = data.species
+      .filter(({ location }) => location === loc)
+      .map(({ name }) => name);
+    result[loc] = animals;
+  });
+  return result;
+};
+
+const getLocationName = () => {
+  const result = {};
+  locations.forEach((loc) => {
+    const locationAnimals = [];
+    data.species
+      .filter(({ location }) => location === loc)
+      .forEach(({ name: speciesName, residents }) => {
+        locationAnimals.push({ [speciesName]: residents.map(({ name }) => name) });
+      });
+    result[loc] = locationAnimals;
+  });
+  return result;
+};
+
+const getLocationSort = (object) => {
+  const { NE, NW, SE, SW } = object;
+  NE.forEach((specie) => {
+    console.log(Object.values(specie).sort());
+    console.log(specie);
+  });
+};
+
+const getLocationSex = (option) => {
+  data.species.forEach(({ residents }, index) => {
+    const filteredResidents = residents.filter(({ sex }) => sex === option);
+    data.species[index].residents = filteredResidents;
+  });
+};
+
+const getLocationWithParameter = (sorted, sex) => {
+  const result = getLocationName();
+  if (sorted) getLocationSort(result);
+  if (sex !== '') getLocationSex(sex);
+  return result;
+};
+
+const getAnimalMap = (options) => {
+  let result = {};
+  if (options === undefined) return getLocation();
+  const { includeNames = false, sorted = false, sex = '' } = options;
+  if (includeNames) {
+    result = getLocationWithParameter(sorted, sex);
+  }
+  return result;
+};
+
+// console.log(getAnimalMap());
+// console.log(getAnimalMap({ includeNames: true }).NE);
+// console.log(getAnimalMap({ includeNames: true, sorted: true }));
+
+const getSchedule = (dayName) => {
+  const schedule = {};
+  Object.keys(data.hours).forEach((day) => {
+    const { open, close } = data.hours[day];
+    if (day === 'Monday') {
+      schedule[day] = 'CLOSED';
+    } else {
+      schedule[day] = `Open from ${open}am until ${close - 12}pm`;
+    }
+  });
+  if (dayName === undefined) return schedule;
+  const result = { [dayName]: schedule[dayName] };
+  return result;
+};
+
+const getOldestFromFirstSpecies = (id) => {
+  const employee = data.employees.find(({ id: employeeId }) => employeeId === id);
+  const firstAnimal = data.species
+    .find(({ id: specieId }) => specieId === employee.responsibleFor[0]);
+  const oldestResident = firstAnimal.residents.sort((a, b) => b.age - a.age)[0];
+  const { name, sex, age } = oldestResident;
+  return [name, sex, age];
+};
 
 function increasePrices(percentage) {
   // seu código aqui
