c4b60decf3b0711725cd3c52c3aa75f502e43d3e
Guilherme Cesconeto <cesconeto@gmail.com>
2021-06-01 17:50:00 -0300

    Linting a 4

--- src/zoo.js
+++ src/zoo.js
@@ -37,7 +37,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith, };
+  return { ...personalInfo, ...associatedWith };
 }
 
 // function isManager(id) {
