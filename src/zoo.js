5b0e5370014e039a73c6dcab924f6c143df4bd11
pedro21medeiros <pedro21medeiros@gmail.com>
2021-06-01 16:58:30 -0300

    tenta requisito 5 - isManager

--- src/zoo.js
+++ src/zoo.js
@@ -33,7 +33,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((professional) => professional.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
