cfda56e8fc1154f26ca9bffc8474fa5a6853e891
Caio César <caiovg32@gmail.com>
2021-06-01 15:48:29 -0300

    req.1

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  
+function getSpeciesByIds(...ids) {
+  if(ids.length === 0) return [];
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
 function getAnimalsOlderThan(animal, age) {
