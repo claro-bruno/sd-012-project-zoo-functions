cf11adbfa252cfed9d512e0dc448bab65ca3ad09
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-06-01 18:15:39 -0300

    exercicio 4 createEmployee.test.js arrumando o lint

--- src/zoo.js
+++ src/zoo.js
@@ -39,7 +39,7 @@ function getEmployeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
-  return {...personalInfo,...associatedWith};
+  return { ...personalInfo, ...associatedWith };
 }
 
 // function isManager(id) {
