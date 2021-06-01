cc713369d66b15fd9ef13a44304810d865fe8b78
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-06-01 15:56:25 -0300

    adicionando requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -28,13 +28,14 @@ function getAnimalsOlderThan(animal, age) {
 function getEmployeeByName(employeeName) {
   // seu código aqui
   if (!employeeName) return {};
-  return employees.find((employee) => 
-  employee.firstName === employeeName ||
-  employee.lastName === employeeName);
+  return employees.find((employee) =>
+    employee.firstName === employeeName
+    || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
