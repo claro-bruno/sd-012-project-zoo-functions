619df038df7127f76485a873039ed1f55a065341
Roberval Filho <rslfilho@gmail.com>
2021-06-01 15:25:49 -0300

    Requisitos 04 e 05

--- src/zoo.js
+++ src/zoo.js
@@ -39,13 +39,18 @@ const getEmployeeByName = (employeeName) => {
   return result;
 };
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+const createEmployee = (personalInfo, associatedWith) => {
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return { id, firstName, lastName, managers, responsibleFor };
+};
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = (id) => {
+  const managerIds = [];
+  data.employees.forEach(({ managers }) => managerIds.push(...managers));
+  const result = managerIds.some((managerId) => managerId === id);
+  return result;
+};
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
