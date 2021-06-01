8b81bcf155e046f71f51fb8521fc9ab1e4e8b08a
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-06-01 15:14:23 -0300

    Inicio do projeto zoo functions

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  Inicio projeto
 }
 
 function getAnimalsOlderThan(animal, age) {
