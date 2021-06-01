8f61715c9085effd12050ee68677b4cea787d600
ViniciusMossi <vini_agg@hotmail.com>
2021-06-01 18:38:11 -0400

    verifica primeiro requisito

--- src/zoo.js
+++ src/zoo.js
@@ -9,72 +9,74 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const { species } = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui teste
-}
-
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
-
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
-
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
-
-function isManager(id) {
-  // seu código aqui
-}
-
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
-
-function countAnimals(species) {
-  // seu código aqui
-}
-
-function calculateEntry(entrants) {
-  // seu código aqui
-}
-
-function getAnimalMap(options) {
-  // seu código aqui
-}
-
-function getSchedule(dayName) {
-  // seu código aqui
-}
-
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
-
-function increasePrices(percentage) {
-  // seu código aqui
-}
-
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
-
-module.exports = {
-  calculateEntry,
-  getSchedule,
-  countAnimals,
-  getAnimalMap,
-  getSpeciesByIds,
-  getEmployeeByName,
-  getEmployeeCoverage,
-  addEmployee,
-  isManager,
-  getAnimalsOlderThan,
-  getOldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+const getSpeciesByIds = (...ids) => {
+  if (!ids) return [];
+  return species.filter((specie) => ids.includes(specie.id));
 };
+console.log(getSpeciesByIds());
+
+// function getAnimalsOlderThan(animal, age) {
+//   // seu código aqui
+// }
+
+// function getEmployeeByName(employeeName) {
+//   // seu código aqui
+// }
+
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
+
+// function isManager(id) {
+//   // seu código aqui
+// }
+
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
+
+// function countAnimals(species) {
+//   // seu código aqui
+// }
+
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
+
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
+
+// function getSchedule(dayName) {
+//   // seu código aqui
+// }
+
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
+
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
+
+// function getEmployeeCoverage(idOrName) {
+//   // seu código aqui
+// }
+
+// module.exports = {
+//   calculateEntry,
+//   getSchedule,
+//   countAnimals,
+//   getAnimalMap,
+//   getSpeciesByIds,
+//   getEmployeeByName,
+//   getEmployeeCoverage,
+//   addEmployee,
+//   isManager,
+//   getAnimalsOlderThan,
+//   getOldestFromFirstSpecies,
+//   increasePrices,
+//   createEmployee,
+// };
