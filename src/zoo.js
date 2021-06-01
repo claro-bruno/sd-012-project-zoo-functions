367a37b7aa901684dbf00e2813c8d4d72a253664
RyanLaiber <ryanlaiber@gmail.com>
2021-06-01 16:23:41 -0300

    adiciona requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,14 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  // ...ids retorna um array de argumentos, caso não seja passado nenhum argumento, ids será um array vazio.
+  if (ids.length === 0) {
+    return [];
+  }
+  return ids.map((element) => species.find((specie) => element === specie.id)); // primeiro é utilizado o método map() para realizar o mesmo procedimento em todos os elementos da lista ids, e restornar um array com os seus retornos listados. dentro do map() é utilizado o método find(), que retorna o primeiro elemento do array species que retorne true na condição passada. OBS: o elemento que é retornado pelo find() é um objeto que está contido dentro da lista species, este objeto é armazenado como o primeiro retorno dado pelo método map().
 }
 
 function getAnimalsOlderThan(animal, age) {
