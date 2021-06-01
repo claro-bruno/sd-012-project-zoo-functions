ba60b6826c8bb2dccf9ba3691a27a66fa7b242dd
Sass <dudu_sass12@hotmail.com>
2021-06-01 15:21:20 -0300

    iniciando o projeto

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  // seu código aquia
 }
 
 function getAnimalsOlderThan(animal, age) {
