5bfdbc05cbbd12e71283068710f87ae15aa3cd37
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-06-01 16:28:31 -0300

    Adiciona requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -40,7 +40,11 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
