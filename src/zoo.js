7e4a281be7425bf99f679c2757eacbd29ba129f3
Tulio <tuliotrindade99@gmail.com>
2021-06-01 19:14:04 -0300

    requisito 5 concluido

--- src/zoo.js
+++ src/zoo.js
@@ -55,11 +55,17 @@ function createEmployee(personalInfo, associatedWith) {
   return novoColaborador;
 }
 
-/*
 function isManager(id) {
-  // seu código aqui
+  if (id === '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  || id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+  || id === '0e7b460e-acf4-4e17-bcb3-ee472265db83') {
+    return true;
+  }
+  return false;
+  
 }
 
+/*
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
@@ -102,8 +108,8 @@ module.exports = {
   /*
   getEmployeeCoverage,
   addEmployee,
-  isManager,
   */
+  isManager,
   getAnimalsOlderThan,
   /*
   getOldestFromFirstSpecies,
