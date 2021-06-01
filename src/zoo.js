c79e0aa623eeaa7d275805ae3a8a313776ba4b40
Milton Castro <castro.milton07@gmail.com>
2021-06-01 12:36:11 -0300

    01 - Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,18 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// const test = data.species.filter((spicie) => spicie.id === '0938aa23-f153-4937-9f88-4858b24d6bce');
+// console.log(test);
+
+function getSpeciesByIds(...ids) {
+  const selectedSpicie = [];
+  ids.forEach((id) => selectedSpicie.push(data.species.find((spicie) => spicie.id === id)));
+  return selectedSpicie;
 }
+
+// console.log(getSpeciesByIds());
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
