bac4fbb7ff522d3dd379f116be3eb1b2765f5225
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-06-01 19:44:41 -0300

    Adequa codigo a regras do linter

--- src/zoo.js
+++ src/zoo.js
@@ -1,18 +1,19 @@
-const assert = require("assert");
+// const assert = require('assert');
 /*
 eslint no-unused-vars: [
-  "error",
+  'error',
   {
-    "args": "none",
-    "vars": "local",
-    "varsIgnorePattern": "data"
+    'args': 'none',
+    'vars': 'local',
+    'varsIgnorePattern': 'data'
   }
 ]
 */
 
-const data = require("./data");
+const data = require('./data');
 
-function getSpeciesByIds(...ids) { // OK
+function getSpeciesByIds(...ids) {
+  // OK
   // seu código aqui
   // if (!ids.length) return [];
   // const getSpeciesById = (id, index) => {
@@ -31,69 +32,71 @@ function getSpeciesByIds(...ids) { // OK
 // const expected = [];
 // assert.deepStrictEqual(actual, expected);
 
-// const actual2 = getSpeciesByIds("0938aa23-f153-4937-9f88-4858b24d6bce");
+// const actual2 = getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 // const expected2 = [
 //   {
-//     id: "0938aa23-f153-4937-9f88-4858b24d6bce",
-//     name: "lions",
+//     id: '0938aa23-f153-4937-9f88-4858b24d6bce',
+//     name: 'lions',
 //     popularity: 4,
-//     location: "NE",
+//     location: 'NE',
 //     residents: [
-//       { name: "Zena", sex: "female", age: 12 },
-//       { name: "Maxwell", sex: "male", age: 15 },
-//       { name: "Faustino", sex: "male", age: 7 },
-//       { name: "Dee", sex: "female", age: 14 },
+//       { name: 'Zena', sex: 'female', age: 12 },
+//       { name: 'Maxwell', sex: 'male', age: 15 },
+//       { name: 'Faustino', sex: 'male', age: 7 },
+//       { name: 'Dee', sex: 'female', age: 14 },
 //     ],
 //   },
 // ];
 // assert.deepStrictEqual(actual2, expected2);
 
 // const actual3 = getSpeciesByIds(
-//   "0938aa23-f153-4937-9f88-4858b24d6bce",
-//   "e8481c1d-42ea-4610-8e11-1752cfc05a46"
+//   '0938aa23-f153-4937-9f88-4858b24d6bce',
+//   'e8481c1d-42ea-4610-8e11-1752cfc05a46'
 // );
 // const expected3 = [
 //   {
-//     id: "0938aa23-f153-4937-9f88-4858b24d6bce",
-//     name: "lions",
+//     id: '0938aa23-f153-4937-9f88-4858b24d6bce',
+//     name: 'lions',
 //     popularity: 4,
-//     location: "NE",
+//     location: 'NE',
 //     residents: [
-//       { name: "Zena", sex: "female", age: 12 },
-//       { name: "Maxwell", sex: "male", age: 15 },
-//       { name: "Faustino", sex: "male", age: 7 },
-//       { name: "Dee", sex: "female", age: 14 },
+//       { name: 'Zena', sex: 'female', age: 12 },
+//       { name: 'Maxwell', sex: 'male', age: 15 },
+//       { name: 'Faustino', sex: 'male', age: 7 },
+//       { name: 'Dee', sex: 'female', age: 14 },
 //     ],
 //   },
 //   {
-//     id: "e8481c1d-42ea-4610-8e11-1752cfc05a46",
-//     name: "tigers",
+//     id: 'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+//     name: 'tigers',
 //     popularity: 5,
-//     location: "NW",
+//     location: 'NW',
 //     residents: [
-//       { name: "Shu", sex: "female", age: 19 },
-//       { name: "Esther", sex: "female", age: 17 },
+//       { name: 'Shu', sex: 'female', age: 19 },
+//       { name: 'Esther', sex: 'female', age: 17 },
 //     ],
 //   },
 // ];
 // assert.deepStrictEqual(actual3, expected3);
 
-function getAnimalsOlderThan(animal, age) { // OK
+function getAnimalsOlderThan(animal, age) {
+  // OK
   // seu código aqui
   return data.species
     .find(({ name }) => name === animal)
     .residents.every((resident) => resident.age > age);
 }
 
-// let actual4 = getAnimalsOlderThan("otters", 7);
+// let actual4 = getAnimalsOlderThan('otters', 7);
 // let expected4 = true;
 // assert.deepStrictEqual(actual4, expected4);
 
-// actual4 = getAnimalsOlderThan("penguins", 10);
+// actual4 = getAnimalsOlderThan('penguins', 10);
 // expected4 = false;
 // assert.deepStrictEqual(actual4, expected4);
 
-function getEmployeeByName(employeeName) { // OK
+function getEmployeeByName(employeeName) {
+  // OK
   // seu código aqui
   if (!employeeName) return {};
   return data.employees.find(
@@ -106,37 +109,38 @@ function getEmployeeByName(employeeName) { // OK
 // const expected5 = {};
 // assert.deepStrictEqual(actual5, expected5);
 
-// const actual6 = getEmployeeByName("Emery");
+// const actual6 = getEmployeeByName('Emery');
 // const expected6 = {
-//   id: "b0dc644a-5335-489b-8a2c-4e086c7819a2",
-//   firstName: "Emery",
-//   lastName: "Elser",
-//   managers: ["9e7d4524-363c-416a-8759-8aa7e50c0992"],
+//   id: 'b0dc644a-5335-489b-8a2c-4e086c7819a2',
+//   firstName: 'Emery',
+//   lastName: 'Elser',
+//   managers: ['9e7d4524-363c-416a-8759-8aa7e50c0992'],
 //   responsibleFor: [
-//     "bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5",
-//     "baa6e93a-f295-44e7-8f70-2bcdc6f6948d",
-//     "0938aa23-f153-4937-9f88-4858b24d6bce",
+//     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+//     'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
+//     '0938aa23-f153-4937-9f88-4858b24d6bce',
 //   ],
 // };
 // assert.deepStrictEqual(actual6, expected6);
 
-// const actual7 = getEmployeeByName("Wishart");
+// const actual7 = getEmployeeByName('Wishart');
 // const expected7 = {
-//   id: "56d43ba3-a5a7-40f6-8dd7-cbb05082383f",
-//   firstName: "Wilburn",
-//   lastName: "Wishart",
+//   id: '56d43ba3-a5a7-40f6-8dd7-cbb05082383f',
+//   firstName: 'Wilburn',
+//   lastName: 'Wishart',
 //   managers: [
-//     "0e7b460e-acf4-4e17-bcb3-ee472265db83",
-//     "fdb2543b-5662-46a7-badc-93d960fdc0a8",
+//     '0e7b460e-acf4-4e17-bcb3-ee472265db83',
+//     'fdb2543b-5662-46a7-badc-93d960fdc0a8',
 //   ],
 //   responsibleFor: [
-//     "78460a91-f4da-4dea-a469-86fd2b8ccc84",
-//     "bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5",
+//     '78460a91-f4da-4dea-a469-86fd2b8ccc84',
+//     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
 //   ],
 // };
 // assert.deepStrictEqual(actual7, expected7);
 
-function createEmployee(personalInfo, associatedWith) { // OK
+function createEmployee(personalInfo, associatedWith) {
+  // OK
   // seu código aqui
   return { ...personalInfo, ...associatedWith };
 }
@@ -175,7 +179,8 @@ function createEmployee(personalInfo, associatedWith) { // OK
 // };
 // assert.deepStrictEqual(createEmployee(personalInfo, associatedWith), expected8);
 
-function isManager(id) { // OK
+function isManager(id) {
+  // OK
   // seu código aqui;
   return data.employees.some((employee) => employee.managers.includes(id));
 }
@@ -188,7 +193,13 @@ function isManager(id) { // OK
 // expected9 = true;
 // assert.deepStrictEqual(actual9, expected9);
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = [],
+) {
   // seu código aqui
   data.employees.push({
     id,
@@ -238,10 +249,11 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 // assert.strictEqual(data.employees.length, 10);
 
-function countAnimals(species) { // OK
+function countAnimals(species) {
+  // OK
   // seu código aqui
   if (!species) {
-    return data.species.reduce((acc, { name, residents }, index) => {
+    return data.species.reduce((acc, { name, residents }) => {
       acc[name] = residents.length;
       return acc;
     }, {});
@@ -273,6 +285,7 @@ function countAnimals(species) { // OK
 
 function calculateEntry(entrants) {
   // seu código aqui
+  return entrants;
 }
 
 // function getAnimalMap(options) {
@@ -296,7 +309,7 @@ function calculateEntry(entrants) {
 // }
 
 module.exports = {
-  // calculateEntry,
+  calculateEntry,
   // getSchedule,
   countAnimals,
   // getAnimalMap,
