70857e1e5d8b7b2cbf74fed9e7ed4364b4175a41
alestori <alestori7@gmail.com>
2021-06-01 14:06:04 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -8,12 +8,14 @@ eslint no-unused-vars: [
   }
 ]
 */
+const { species } = require('./data');
+// const data = require('./data');
 
-const data = require('./data');
-
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return species.filter((specie) => ids.some((id) => specie.id === id));
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
