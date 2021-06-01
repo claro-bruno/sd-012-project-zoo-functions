2fde931f7fc9bf4516b175f744d85cb978a11ace
matheustkaczyk <matheus111904@hotmail.com>
2021-06-01 18:18:06 -0300

    requesito 2 concluido

--- src/zoo.js
+++ src/zoo.js
@@ -19,11 +19,13 @@ function getSpeciesByIds(...ids) {
   const pegaEspecie = species.filter((sId, index) => sId.id === ids[index]);
   return pegaEspecie;
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const idadeMinima = species.find((ani) => ani.name === animal).residents.every((ani) => ani.age >= age);
+  return idadeMinima;
 }
+// console.log(getAnimalsOlderThan('otters', 7))
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
