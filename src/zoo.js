2db05becefb9bfd014ad57f08206b448036492aa
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-06-01 18:04:15 -0300

    Requisito 3 com lint

--- src/zoo.js
+++ src/zoo.js
@@ -17,52 +17,52 @@ function getSpeciesByIds(...animals) {
 
 function getAnimalsOlderThan(animal, age) {
   return species.find((specie) => specie.name === animal)
-  .residents.every((animals) => animals.age >= age)
+    .residents.every((animals) => animals.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
-  return employees.filter((employee)
+  return employeeName ? employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName) : {};
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
 
-function isManager(id) {
-  // seu código aqui
-}
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   seu código aqui
+// }
 
-function countAnimals(species) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   seu código aqui
+// }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+// function countAnimals(species) {
+//   seu código aqui
+// }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+// function calculateEntry(entrants) {
+//   seu código aqui
+// }
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+// function getAnimalMap(options) {
+//   seu código aqui
+// }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function getSchedule(dayName) {
+//   seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function getOldestFromFirstSpecies(id) {
+//   seu código aqui
+// }
 
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   seu código aqui
+// }
+
+// function getEmployeeCoverage(idOrName) {
+//   seu código aqui
+// }
 
 module.exports = {
   calculateEntry,
