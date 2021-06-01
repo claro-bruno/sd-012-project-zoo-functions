3c159756edb4426c284dde7c18dd79120b2b4ef0
camila-mp <ca.malvp@gmail.com>
2021-06-01 16:38:40 -0300

    comentando os outros req

--- src/zoo.js
+++ src/zoo.js
@@ -18,55 +18,53 @@ function getSpeciesByIds(...ids) {
   })
 };
 
-console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'))
+// function getAnimalsOlderThan(animal, age) {
+//   // seu código aqui
+// }
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// function getEmployeeByName(employeeName) {
+//   // seu código aqui
+// }
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function countAnimals(species) {
+//   // seu código aqui
+// }
 
-function countAnimals(species) {
-  // seu código aqui
-}
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+// function getSchedule(dayName) {
+//   // seu código aqui
+// }
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
-
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function getEmployeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
   calculateEntry,
