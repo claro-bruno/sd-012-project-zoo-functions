cb3644a2a29f75fa613c07c351fe84158eb5ee6d
Stefano <stefanooliveira1@gmail.com>
2021-06-01 18:03:35 -0300

    Requisito 4 - finalizado

--- src/zoo.js
+++ src/zoo.js
@@ -44,6 +44,12 @@ function getEmployeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const newEmployee = {
+     ...personalInfo,
+     ...associatedWith
+  };
+
+  return newEmployee
 }
 
 function isManager(id) {
