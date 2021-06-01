6440cd8472a80a78bec2b30c609905ea8e64e6d6
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-01 19:24:07 -0300

    Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -35,7 +35,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return {...personalInfo, ...associatedWith}
 }
 
 function isManager(id) {
