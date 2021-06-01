437578fef5d84841c7af07fca3e66666cda85e68
Martin <escorpmartin97@gmail.com>
2021-06-01 17:55:42 -0300

    req 2

--- src/zoo.js
+++ src/zoo.js
@@ -17,65 +17,65 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find((ani) => ani.name === animal).residents.every((a) => a.age > age);
 }
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+// function getEmployeeByName(employeeName) {
+//   // seu código aqui
+// }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function countAnimals(species) {
-  // seu código aqui
-}
+// function countAnimals(species) {
+//   // seu código aqui
+// }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+// function getSchedule(dayName) {
+//   // seu código aqui
+// }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function getEmployeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
-  calculateEntry,
-  getSchedule,
-  countAnimals,
-  getAnimalMap,
+  // calculateEntry,
+  // getSchedule,
+  // countAnimals,
+  // getAnimalMap,
   getSpeciesByIds,
-  getEmployeeByName,
-  getEmployeeCoverage,
-  addEmployee,
-  isManager,
+  // getEmployeeByName,
+  // getEmployeeCoverage,
+  // addEmployee,
+  // isManager,
   getAnimalsOlderThan,
-  getOldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+  // getOldestFromFirstSpecies,
+  // increasePrices,
+  // createEmployee,
 };
