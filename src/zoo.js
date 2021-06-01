c51e9b5a51ed749dfbdf2e201b6def7e395eef41
pedro21medeiros <pedro21medeiros@gmail.com>
2021-06-01 15:14:27 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  // seu código aqui 
 }
 
 function getAnimalsOlderThan(animal, age) {
