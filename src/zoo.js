a7df09e0c46135bbf424db9890826227303095c8
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-06-01 18:08:52 -0300

    Resolvendo requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,14 @@ eslint no-unused-vars: [
 ]
 */
 
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui 
+function getSpeciesByIds(...ids) {
+  const retorno = ids.length === 0 ? [] : ids.map((id) => species.find((specie) => specie.id === id));
+  return retorno;
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
