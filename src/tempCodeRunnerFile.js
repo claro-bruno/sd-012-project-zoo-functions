b665565bec28bba63f1af90a4a932f49c8990747
Martin <escorpmartin97@gmail.com>
2021-06-01 17:58:43 -0300

    correcao req 1 e 2

--- src/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-const data = require('./data');
-
-function getSpeciesByIds(...ids) {
-  const { animals } = data;
-  // seu código aqui
-  const result = ids.map((id) => animals.find((animal) => animal.id === id));
-  return result;
