e25f246e3df46086cdfc9e0e88859e8d8e8d8b85
matheustkaczyk <matheus111904@hotmail.com>
2021-06-01 19:37:12 -0300

    requesito 3 concluido

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { species } = require('./data');
+const { species, employees } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -22,54 +22,61 @@ function getSpeciesByIds(...ids) {
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
-  const idadeMinima = species.find((ani) => ani.name === animal).residents.every((ani) => ani.age >= age);
+  const idadeMinima = species.find((ani) => ani.name === animal)
+  .residents.every((ani) => ani.age >= age);
   return idadeMinima;
 }
 // console.log(getAnimalsOlderThan('otters', 7))
 
 function getEmployeeByName(employeeName) {
-  
+  if (!employeeName) {
+    return {};
+  }
+  const achaFunc = employees.find((func) => func.firstName === employeeName || func.lastName === employeeName);
+  return achaFunc;
+}
+console.log(getEmployeeByName('Wishart'))
+
+
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
 }
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  // seu código aqui
+}
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  // seu código aqui
+}
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
+function countAnimals(species) {
+  // seu código aqui
+}
 
-// function countAnimals(species) {
-//   // seu código aqui
-// }
+function calculateEntry(entrants) {
+  // seu código aqui
+}
 
-// function calculateEntry(entrants) {
-//   // seu código aqui
-// }
+function getAnimalMap(options) {
+  // seu código aqui
+}
 
-// function getAnimalMap(options) {
-//   // seu código aqui
-// }
+function getSchedule(dayName) {
+  // seu código aqui
+}
 
-// function getSchedule(dayName) {
-//   // seu código aqui
-// }
+function getOldestFromFirstSpecies(id) {
+  // seu código aqui
+}
 
-// function getOldestFromFirstSpecies(id) {
-//   // seu código aqui
-// }
+function increasePrices(percentage) {
+  // seu código aqui
+}
 
-// function increasePrices(percentage) {
-//   // seu código aqui
-// }
-
-// function getEmployeeCoverage(idOrName) {
-//   // seu código aqui
-// }
+function getEmployeeCoverage(idOrName) {
+  // seu código aqui
+}
 
 module.exports = {
   calculateEntry,
