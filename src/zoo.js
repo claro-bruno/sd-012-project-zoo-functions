7183733999bbe892f36ec043a23b87dececb7fbc
Pedro Gonçalves <peg.gep27@gmail.com>
2021-06-01 16:12:29 -0300

    lint requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -29,8 +29,8 @@ const getAnimalsOlderThan = (animal, age) => {
 
 const getEmployeeByName = (employeeName) => {
   if (employeeName !== undefined) {
-    const [obj] = employees.find((employee) => employee.firstName === employeeName ||
-    employee.lastName === employeeName);
+    const [obj] = employees.find((employee) => employee.firstName === employeeName
+    || employee.lastName === employeeName);
     return obj;
   }
   return {};
