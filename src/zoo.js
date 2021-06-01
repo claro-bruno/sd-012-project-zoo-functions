11bbae7e7771fb9e40f3427b8bec03fb37d0a947
Augusto <augustocm2002@yahoo.com.br>
2021-06-01 15:19:29 -0300

    Iniciando o projeto

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  // vamos começar!
 }
 
 function getAnimalsOlderThan(animal, age) {
