bc2450dd287d71983f792fc842978705d789519b
Euller Braz <euller.braz99@gmail.com>
2021-06-01 16:46:22 -0300

    Resolvido Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -29,9 +29,9 @@ function getEmployeeByName(employeeName) {
   }, {});
 }
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+function createEmployee(personalInfo, associatedWith) {
+  return { ...personalInfo, ...associatedWith };
+}
 
 // function isManager(id) {
 //   // seu código aqui
@@ -82,5 +82,5 @@ module.exports = {
   getAnimalsOlderThan,
   // getOldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };
