9838ea270c063e89af9b73b6adad9c317360cc8f
Samuel Constantino <samuelicapuidados@gmail.com>
2021-06-01 21:11:00 -0300

    implementa requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -48,9 +48,14 @@ function isManager(id) {
   return true;
 }
 
-function addEmployee() {
-  // id, firstName, lastName, managers, responsibleFor
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function countAnimals() {
