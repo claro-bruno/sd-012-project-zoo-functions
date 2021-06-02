c031ff6d741d95a6b21f2eac302aa686a626dfba
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-02 16:15:05 -0300

    Resolve requisito 5 e lint

--- src/zoo.js
+++ src/zoo.js
@@ -38,31 +38,16 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return `{ ${personalInfo.firstName} }`;
+  return { ...personalInfo, ...associatedWith };
 }
 
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992',
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-  ],
-};
-
-console.log(createEmployee(personalInfo, associatedWith));
-
 function isManager(id) {
-  // seu código aqui
+  return employees
+    .reduce((acc, curr) => {
+      acc.push(...curr.managers);
+      return acc;
+    }, [])
+    .some((manager) => manager === id);
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
