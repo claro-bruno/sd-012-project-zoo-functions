c732920150902fcc5b6bb34b230b1e13d199dc0e
ArturAlvaro <artur.alvaro73@gmail.com>
2021-06-01 23:30:32 -0300

    Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -33,20 +33,44 @@ function getEmployeeByName(employeeName) {
   return employee;
 }
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-//   const newUser = (id, firstName, lastName) => ({
-//     id,
-//     firstName,
-//     lastName,
-//   });
-//   return newUser;
-// }
-// console.log(createEmployee());
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+  const obj = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor,
+  };
+  return obj;
+}
+console.log(
+  createEmployee(
+    {
+      id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+    {
+      managers: [
+        'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+        '9e7d4524-363c-416a-8759-8aa7e50c0992',
+      ],
+      responsibleFor: [
+        '0938aa23-f153-4937-9f88-4858b24d6bce',
+        '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+        'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+      ],
+    },
+  ),
+);
 
 // function isManager(id) {
 //   // seu código aqui
+//   const manager = employees.filter((manage) => manage.id === id);
+//   return manager;
 // }
+// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -93,5 +117,5 @@ module.exports = {
   getAnimalsOlderThan,
   // getOldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };
