42533b76558ea6f9a4e4f24d7d57ce16fcafe765
PeDD1 <pedrohr0909@gmail.com>
2021-06-02 15:22:23 -0300

    Requisito 1 realizado sem lint

--- src/zoo.js
+++ src/zoo.js
@@ -10,53 +10,59 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const {
+  species,
+  employees,
+  hours,
+  prices,
+} = data;
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((specie) => ids.some((id) => specie.id === id));
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+
 }
 
 function isManager(id) {
-  // seu código aqui
+  
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  
 }
 
 function countAnimals(species) {
-  // seu código aqui
+  
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  
 }
 
 function getAnimalMap(options) {
-  // seu código aqui
+  
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  
 }
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  
 }
 
 function getEmployeeCoverage(idOrName) {
