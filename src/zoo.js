f088bdf8737a43cc8dca68082ed37d7b5b91a58d
Álvaro Palma <palma.alvaro@outlook.com>
2021-06-02 16:18:04 -0300

    Arruma lint linha 20 requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -10,17 +10,15 @@ eslint no-unused-vars: [
 */
 
 const { species } = require('./data');
-const data = require('./data');
+// const data = require('./data');
 
 function getSpeciesByIds(...ids) {
   if (ids === null) {
     return [];
   }
   const rightId = ids;
-  return data.species.filter((specie) => specie.id === rightId.find((id) => id === specie.id ) );
+  return species.filter((specie) => specie.id === rightId.find((id) => id === specie.id));
 }
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan() {
   // seu código aqui animal, age
