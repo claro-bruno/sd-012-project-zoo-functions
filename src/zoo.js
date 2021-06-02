866b682e71e003d9e8d5cf218b303aead9d31712
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-02 18:34:03 -0300

    (12) - Finaliza getEmployeeCoverage

--- src/zoo.js
+++ src/zoo.js
@@ -123,9 +123,28 @@ function increasePrices(percentage) {
   });
 }
 
-// function getEmployeeCoverage(idOrName) {
-//   // seu código aqui
-// }
+function getEmployeeCoverage(idOrName) {
+  let employees = data.employees.reduce((acc, { id, firstName, lastName }) => {
+    const speciesId = data.employees.find((employee) => employee.id === id).responsibleFor;
+    const animals = getSpeciesByIds(...speciesId);
+    const residents = animals.reduce((count, curr) => { count.push(curr.name); return count; }, []);
+    acc[`${firstName} ${lastName}`] = residents;
+    return acc;
+  }, {});
+  const employeesNames = Object.keys(employees);
+  let employeeName;
+  if (data.employees.some((employee) => employee.id === idOrName)) {
+    employeeName = data.employees.find((element) => element.id === idOrName).firstName;
+  }
+  employeesNames.forEach((name) => {
+    if (name.includes(idOrName) || name.includes(employeeName)) {
+      employees = { [name]: employees[name] };
+    }
+  });
+  return employees;
+}
+
+console.log(getEmployeeCoverage('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = {
   calculateEntry,
@@ -134,7 +153,7 @@ module.exports = {
   // getAnimalMap,
   getSpeciesByIds,
   getEmployeeByName,
-  //   getEmployeeCoverage,
+  getEmployeeCoverage,
   addEmployee,
   isManager,
   getAnimalsOlderThan,
