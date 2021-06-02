635df7b0384c6a519348d9a38fdb1c28375315ef
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-06-02 20:04:57 -0300

    lint do req1

--- src/zoo.js
+++ src/zoo.js
@@ -20,11 +20,9 @@ function getSpeciesByIds(...ids) {
     return [];
   }
   /* result = species.filter((spec) => spec.id === ids) */
-  const identidade = species.filter((spec, indice) => spec.id === ids[indice]); 
+  const identidade = species.filter((spec, indice) => spec.id === ids[indice]);
   return identidade;
 }
-getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
