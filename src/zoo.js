b2d5aa7be112f2b5af8043433450246c1ef12794
Euller Braz <euller.braz99@gmail.com>
2021-06-02 17:51:31 -0300

    Resolvido Requisito 9

--- src/zoo.js
+++ src/zoo.js
@@ -59,9 +59,90 @@ function calculateEntry(entrants) {
   return adultPrice * adultTot + seniorPrice * seniorTot + childPrice * childTot;
 }
 
-// function getAnimalMap(options) {
-//   // seu código aqui
-// }
+// Requisito 9
+
+const animalMapNoParameter = (regions) => {
+  regions.NE.push(...data.species
+    .filter((specie) => specie.location === 'NE')
+    .map((specie) => specie.name));
+  regions.NW.push(...data.species
+    .filter((specie) => specie.location === 'NW')
+    .map((specie) => specie.name));
+  regions.SE.push(...data.species
+    .filter((specie) => specie.location === 'SE')
+    .map((specie) => specie.name));
+  regions.SW.push(...data.species
+    .filter((specie) => specie.location === 'SW')
+    .map((specie) => specie.name));
+  return regions;
+};
+
+const animalMapInclude = (regions) => {
+  regions.NE.push(...data.species
+    .filter((specie) => specie.location === 'NE')
+    .map((specie) => ({ [specie.name]: specie.residents
+      .map((resident) => resident.name) })));
+  regions.NW.push(...data.species
+    .filter((specie) => specie.location === 'NW')
+    .map((specie) => ({ [specie.name]: specie.residents
+      .map((resident) => resident.name) })));
+  regions.SE.push(...data.species
+    .filter((specie) => specie.location === 'SE')
+    .map((specie) => ({ [specie.name]: specie.residents
+      .map((resident) => resident.name) })));
+  regions.SW.push(...data.species
+    .filter((specie) => specie.location === 'SW')
+    .map((specie) => ({ [specie.name]: specie.residents
+      .map((resident) => resident.name) })));
+  return regions;
+};
+
+const animalMapSorted = (regions, sorted) => {
+  if (sorted) {
+    regions.NE.forEach((specie) => Object.values(specie)[0].sort());
+    regions.NW.forEach((specie) => Object.values(specie)[0].sort());
+    regions.SE.forEach((specie) => Object.values(specie)[0].sort());
+    regions.SW.forEach((specie) => Object.values(specie)[0].sort());
+  }
+  return regions;
+};
+
+const sexPerRegion = (regions, region, sex) =>
+  regions[region].map((specieReg) => {
+    const filterResidents = Object.values(specieReg)[0]
+      .filter((residentName) => {
+        const residentObj = data.species.find((specieRes) => specieRes.name
+   === Object.keys(specieReg)[0]).residents.find((resident) => resident.name === residentName);
+        return residentObj.sex === sex;
+      });
+    return { [Object.keys(specieReg)[0]]: filterResidents };
+  });
+
+const animalMapSex = (regions, sex) => {
+  if (sex) {
+    const newReg = {
+      NE: sexPerRegion(regions, 'NE', sex),
+      NW: sexPerRegion(regions, 'NW', sex),
+      SE: sexPerRegion(regions, 'SE', sex),
+      SW: sexPerRegion(regions, 'SW', sex),
+    };
+    return newReg;
+  }
+  return regions;
+};
+
+function getAnimalMap(options) {
+  const regions = data.species.reduce((acc, currentValue) =>
+    Object.assign(acc, { [currentValue.location]: [] }), {});
+  if (!options) return animalMapNoParameter(regions);
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) {
+    animalMapInclude(regions);
+    animalMapSorted(regions, sorted);
+    return animalMapSex(regions, sex);
+  }
+  return animalMapNoParameter(regions);
+}
 
 // function getSchedule(dayName) {
 //   // seu código aqui
@@ -83,7 +164,7 @@ module.exports = {
   calculateEntry,
   // getSchedule,
   countAnimals,
-  // getAnimalMap,
+  getAnimalMap,
   getSpeciesByIds,
   getEmployeeByName,
   // getEmployeeCoverage,
@@ -94,3 +175,10 @@ module.exports = {
   // increasePrices,
   createEmployee,
 };
+
+/* newReg.NE = regions.NE.map((specieReg) => Object.values(specieReg)[0]
+.filter((residentName) => {
+  const residentObj = data.species.find((specieRes) => specieRes.name
+ === Object.keys(specieReg)[0]).residents.find((resident) => resident.name === residentName);
+  return residentObj.sex === sex;
+})); */
