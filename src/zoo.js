db005502eb000cceb43245ed5034bf3dca0a47a1
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-06-02 18:41:45 -0300

    Resolve o lint

--- src/zoo.js
+++ src/zoo.js
@@ -12,12 +12,10 @@ eslint no-unused-vars: [
 const { species, employees } = require('./data');
 const data = require('./data');
 
-//function getSpeciesByIds(...ids) {
-
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) {
-    return []
-  };
+    return [];
+  }
   return species.filter((specie, index) =>
     specie.id === ids[index]);
 }
@@ -29,18 +27,19 @@ function getAnimalsOlderThan(animal, age) {
 
 function getEmployeeByName(employeeName) {
   if (!employeeName) {
-    return {}
-  };
+    return {};
+  }
 
-  const worker = employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
-  return worker
+  const worker = employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return worker;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   const newEmployee = {
     ...personalInfo,
     ...associatedWith,
-  }
+  };
   return newEmployee;
 }
 
@@ -56,17 +55,20 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
     lastName,
     managers,
     responsibleFor,
-  }
+  };
   return employees.push(newEmployee);
 }
 
-function countAnimals(species) {
-  if (!species) {
-    const allAnimals = {}
-    data.species.forEach((animal) => allAnimals[animal.name] = animal.residents.length);
+function countAnimals(speciesPar) {
+  if (!speciesPar) {
+    const allAnimals = {};
+    data.species.forEach((animal) => {
+      allAnimals[animal.name] = animal.residents.length;
+      return true;
+    });
     return allAnimals;
   }
-  return data.species.find((animal) => animal.name === species).residents.length;
+  return data.species.find((animal) => animal.name === speciesPar).residents.length;
 }
 
 function calculateEntry(...entrants) {
@@ -74,40 +76,68 @@ function calculateEntry(...entrants) {
     return 0;
   }
 
-  if(Object.keys(...entrants).length === 0) {
+  if (Object.keys(...entrants).length === 0) {
     return 0;
   }
 
   const [people] = entrants;
 
-  const {Adult:adultNum = 0, Child:childrenNum = 0, Senior:seniorNum = 0} = people;
+  const { Adult: adultNum = 0, Child: childrenNum = 0, Senior: seniorNum = 0 } = people;
 
-  const prices = (data.prices);
+  const { Adult: adultPrice, Senior: seniorPrice, Child: childPrice } = data.prices;
 
-  const {Adult:adultPrice, Senior:seniorPrice, Child:childPrice} = prices;
-
-  return adultNum * adultPrice + childrenNum * childPrice + seniorNum * seniorPrice;   
+  return adultNum * adultPrice + childrenNum * childPrice + seniorNum * seniorPrice;
 }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+// function getSchedule(dayName) {
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+//   // const schedule = {
+//   //   'Tuesday': 'Open from 8am until 6pm',
+//   //   'Wednesday': 'Open from 8am until 6pm',
+//   //   'Thursday': 'Open from 10am until 8pm',
+//   //   'Friday': 'Open from 10am until 8pm',
+//   //   'Saturday': 'Open from 8am until 10pm',
+//   //   'Sunday': 'Open from 8am until 8pm',
+//   //   'Monday': 'CLOSED'
+//   // }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+//   // if(!dayName) {
+//   //   return schedule;
+//   // }
 
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+//   // return 
+
+//   // const fullSchedule = data.hours;
+
+//   // console.log(fullSchedule, 'datas');
+
+//   // const {Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Monday} = fullSchedule;
+
+//   // console.log(Tuesday, 'horario');
+
+
+
+//   // if(dayName = 0){
+//   //   return 
+//   // }
+
+// }
+
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
+
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
+
+// function getEmployeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
   calculateEntry,
