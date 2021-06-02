a08f27a0346af8859822fc93f6f25fa33d47fb80
Patricia Silvestre <patriciasilvestrea@gmail.com>
2021-06-02 09:34:25 -0300

    requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -14,7 +14,10 @@ const data = require('./data');
 function getSpeciesByIds(...ids) {
   return ids.map((id) => data.species.find((animalsId) => animalsId.id === id));
 } // consultei o repositório da Camila Malvess e do kevin Oliveira para ajudar no meu raciocínio
-console.log(getSpeciesByIds());
+// console.log(getSpeciesByIds());
+// console.log((getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce')));
+console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46', '01422318-ca2d-46b8-b66c-3e9e188244ed'));
 
 function getAnimalsOlderThan(animal, age) {
   return data.species.find((animalName) => animalName.name === animal)
@@ -22,9 +25,16 @@ function getAnimalsOlderThan(animal, age) {
 }
 console.log((getAnimalsOlderThan('lions', 3)));
 
-// function getEmployeeByName(employeeName) {
-// // seu código aqui
-// }
+function getEmployeeByName(employeeName) {
+  const findEmployee = data.employees.find((firstAndLastName) => firstAndLastName.firstName
+  === employeeName || firstAndLastName.lastName === employeeName);
+  if (findEmployee === undefined) {
+    return {};
+  }
+  return findEmployee;
+}
+// console.log((getEmployeeByName('Nigel')));
+console.log((getEmployeeByName()));
 
 // function createEmployee(personalInfo, associatedWith) {
 // // seu código aqui
@@ -72,7 +82,7 @@ module.exports = {
   // countAnimals,
   // getAnimalMap,
   getSpeciesByIds,
-  // getEmployeeByName,
+  getEmployeeByName,
   // getEmployeeCoverage,
   // addEmployee,
   // isManager,
