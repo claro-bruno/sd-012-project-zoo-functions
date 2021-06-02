afbdea326b35ce5b478f7a2fee9c72835c4287e2
thales <thalesdaiel2013@gmail.com>
2021-06-02 15:36:00 -0300

    corrigido o lint do ultimo commit

--- src/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const data = require('./data');
-
-function getSpeciesByIds(...ids) {
-
-  return data.species.filter((specie, index) => specie.id === ids[index]);
-  
-  }
-  
-  console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
--- src/teste.js
+++
@@ -1,8 +0,0 @@
-const data = require('./data');
-
-function getEmployeeByName(nome) {
-  const retorno = data.employees.find((employee) => employee.firstName === nome || employee.lastName === nome);
-  return nome.length === 0 ? [] : retorno;
-}
-
-console.log(getEmployeeByName('Nelson'))
