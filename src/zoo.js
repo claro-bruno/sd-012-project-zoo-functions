5f7286b4b6bcfb201fe230920c2dbb57ef9d751e
Teika <shimworks.tk@gmail.com>
2021-06-01 22:57:32 -0300

    pula resquisito 9 e resolve requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -63,11 +63,30 @@ function calculateEntry(entrants = 0) {
 }
 
 function getAnimalMap(options) {
-  // seu código aqui
+  // const regioes = {
+  //   NE: [],
+  //   NW: [],
+  //   SE: [],
+  //   SW: [],
+  // };
+  // species.map((elem) => regioes[elem.location].push(elem.name));
+  // species.forEach((animal) => regioes[animal.location] = species.map((elem) =>))
+  // return regioes;
 }
+// console.log(getAnimalMap());
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const schedule = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+
+  return dayName === undefined ? schedule : { [dayName]: schedule[dayName] };
 }
 
 function getOldestFromFirstSpecies(id) {
