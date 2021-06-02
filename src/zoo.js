388b7dff098c12796f6fd7c66fc7aeece6c7db25
Elias Forte <eliasforte.dev@gmail.com>
2021-06-02 16:32:13 -0300

    comentando a funçao isManager

--- src/zoo.js
+++ src/zoo.js
@@ -36,11 +36,11 @@ function createEmployee(personalInfo, associatedWith) {
   return CreateNewEmployee;
 }
 
-function isManager(id) {
-  const findIdManager = employees.filter((collaborator) => collaborator.id === id)
-  const verifyManager = findIdManager.some((person) => person.managers.includes === id);
-  return findIdManager;
-}
+// function isManager(id) {
+//   const findIdManager = employees.filter((collaborator) => collaborator.id === id)
+//   const verifyManager = findIdManager.some((person) => person.managers.includes === id);
+//   return findIdManager;
+// }
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -83,7 +83,7 @@ module.exports = {
   getEmployeeByName,
   // getEmployeeCoverage,
   // addEmployee,
-  isManager,
+  // isManager,
   getAnimalsOlderThan,
   // getOldestFromFirstSpecies,
   // increasePrices,
