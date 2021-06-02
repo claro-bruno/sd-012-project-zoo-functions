bb1e4cd4a111b88953e166b157cd416322c09a2e
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-02 19:35:01 -0300

    Realiza requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -30,7 +30,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
