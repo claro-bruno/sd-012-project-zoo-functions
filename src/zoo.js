45b36c4c8a8c38735e7d3ed1ab1e2030c8a75e1b
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-02 15:58:29 -0300

    Resolve requisito 3 e lint

--- src/zoo.js
+++ src/zoo.js
@@ -20,7 +20,13 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specific = species
+    .filter((specie) => specie.name === animal)
+    .reduce((acc, curr) => {
+      acc.push(curr.residents);
+      return acc;
+    }, []);
+  return specific[0].every((resident) => resident.age > age);
 }
 
 function getEmployeeByName(employeeName) {
@@ -32,8 +38,28 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return `{ ${personalInfo.firstName} }`;
 }
+
+const personalInfo = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+const associatedWith = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992',
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+  ],
+};
+
+console.log(createEmployee(personalInfo, associatedWith));
 
 function isManager(id) {
   // seu código aqui
