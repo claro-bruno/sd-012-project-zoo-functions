e02ab28e037715cb92d903651a728a6e12ccff11
Milton Castro <castro.milton07@gmail.com>
2021-06-01 22:56:49 -0300

    08 - Requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -1,16 +1,4 @@
-/* eslint no-unused-vars: [
-  "error",
-  {
-    "args": "none",
-    "vars": "local",
-    "varsIgnorePattern": "data",
-  }
-] */
-
 const data = require('./data');
-
-// const test = data.species.filter((spicie) => spicie.id === '0938aa23-f153-4937-9f88-4858b24d6bce');
-// console.log(test);
 
 function getSpeciesByIds(...ids) {
   const selectedSpecie = [];
@@ -18,17 +6,10 @@ function getSpeciesByIds(...ids) {
   return selectedSpecie;
 }
 
-// console.log(getSpeciesByIds());
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
-
 function getAnimalsOlderThan(animal, age) {
   const selectedSpecie = data.species.find((specie) => specie.name === animal);
   return selectedSpecie.residents.every((resident) => resident.age >= age);
 }
-
-// console.log(getAnimalsOlderThan('otters', 7));
-// console.log(getAnimalsOlderThan('penguins', 10));
 
 function getEmployeeByName(employeeName) {
   if (typeof (employeeName) !== 'undefined') {
@@ -39,10 +20,6 @@ function getEmployeeByName(employeeName) {
   } return {};
 }
 
-// console.log(getEmployeeByName());
-// console.log(getEmployeeByName('Emery'));
-// console.log(getEmployeeByName('Wishart'));
-
 function createEmployee(personalInfo, associatedWith) {
   const newEmployee = {
     id: personalInfo.id,
@@ -51,29 +28,8 @@ function createEmployee(personalInfo, associatedWith) {
     managers: associatedWith.managers,
     responsibleFor: associatedWith.responsibleFor,
   };
-  // data.employees.push(newEmployee);
   return newEmployee;
 }
-
-/* const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992',
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-  ],
-}; */
-
-// console.log(createEmployee(personalInfo, associatedWith));
 
 function isManager(id) {
   let checkManager = false;
@@ -83,54 +39,9 @@ function isManager(id) {
   return checkManager;
 }
 
-// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
-// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
-
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  // const newEmployee = { id, firstName, lastName, managers, responsibleFor };
-  // data.employees.push(newEmployee);
   return data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
-
-/* console.log(addEmployee('39800c14-4b76-454a-858d-2f8d168146a7', 'John', 'Doe'));
-console.log(addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
-  [
-    '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
-    'a67a36ee-3765-4c74-8e0f-13f881f6588a',
-  ],
-  [
-    'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
-    '210fcd23-aa7b-4975-91b7-0230ebb27b99',
-  ])); */
-// console.log(data.employees);
-
-/* const createAnimalsObject = (animalsArray, defaultObject) => {
-  const animalObject = defaultObject;
-  animalObject[Object.keys(animalsArray)] = `${Object.values(animalsArray)}`;
-  return animalObject;
-}; */
-
-/* function countAnimals(species) {
-  if (typeof (species) === 'undefined') {
-    const defaultAnimals = data.species.map((specie) => ({ [specie.name]: specie.popularity }));
-    const defaultAnimalsObject = {};
-    defaultAnimals.forEach((animal) => createAnimalsObject(animal, defaultAnimalsObject));
-    return defaultAnimalsObject;
-  }
-} */
-
-/* function countAnimals(species) {
-  if (typeof (species) === 'undefined') {
-    const animalsObject = {};
-    const defaultAnimals = data.species.map((specie) => {
-      const newObjectAnimals = { animal: specie.name, quantity: specie.residents.length };
-      return newObjectAnimals;
-    });
-    const animalsObjectFormat = ({ animal, quantity }) => animalsObject[animal] = quantity;
-    defaultAnimals.forEach((animal) => animalsObjectFormat(animal));
-    return animalsObject;
-  }
-} */
 
 function countAnimals(species) {
   if (typeof (species) === 'undefined') {
@@ -139,13 +50,16 @@ function countAnimals(species) {
   } return data.species.find((specie) => specie.name === species).residents.length;
 }
 
-// console.log(countAnimals());
-// console.log(countAnimals('lions'));
-// console.log(countAnimals('snakes'));
+const sumEntry = (entrants) => entrants.reduce((prevValue, currValue) => {
+  if (currValue[0] === 'Adult') return prevValue + (currValue[1] * 49.99);
+  if (currValue[0] === 'Senior') return prevValue + (currValue[1] * 24.99);
+  return prevValue + (currValue[1] * 20.99);
+}, 0);
 
-/* function calculateEntry(entrants) {
-  // seu código aqui
-} */
+function calculateEntry(entrants) {
+  if (typeof (entrants) === 'undefined' || Object.keys(entrants).length === 0) return 0;
+  return sumEntry(Object.entries(entrants));
+}
 
 /* function getAnimalMap(options) {
   // seu código aqui
@@ -168,7 +82,7 @@ function countAnimals(species) {
 } */
 
 module.exports = {
-  // calculateEntry,
+  calculateEntry,
   // getSchedule,
   countAnimals,
   // getAnimalMap,
