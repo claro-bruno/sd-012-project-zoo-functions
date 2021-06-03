f00df433ee6849f61db584aa19acad9e5bbb8c9f
gscuri <gscuri6@hotmail.com>
2021-06-03 01:49:18 -0300

    oo gloria, primeiro exercicio feito!

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,14 @@ eslint no-unused-vars: [
 ]
 */
 
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui inciando
+function getSpeciesByIds(...ids) {
+  return species.filter((valor) => ids.find((valueIds) => valor.id === valueIds));
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce','bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
