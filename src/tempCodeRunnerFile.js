32c98290af8c0c5f36c098521fd0a376ed1e483b
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-06-03 20:05:52 -0300

    Requisito 8.calculateEntry

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,6 +1,14 @@
-function isManager(id) {
+function calculateEntry(entrants) {
   // seu código aqui
-  return employees.some((employee) => employee.managers.find((idManager) => idManager === id));
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  
+  // const precos = Object.values(prices);
+  // const visits = Object.values(entrants);
+  // console.log(precos);
+  // console.log(visits);
+  // return (precos[0] * visits[0]) + (precos[1] * visits[1]) + (precos[2] * visits[2]);
+ const {Adult = 0, Senior = 0, Child = 0} = entrants
+ const {Adult: adultPrice, Senior: seniorPrice, Child: childPrice} = prices
+ return (Adult * adultPrice) + (Senior * seniorPrice) + (Child * childPrice);
 }
-
-console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+console.log(calculateEntry({ 'Adult': 2, 'Senior': 3, 'Child': 1 }));
--- src/zoo.js
+++ src/zoo.js
@@ -9,14 +9,13 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
-const { species, employees } = require('./data');
+const { species, employees, prices } = require('./data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
   const findId = species.filter((specie) => ids.find((id) => id === specie.id)); // O filter já retorna um array vazio se nçao recebe nenhum parâmetro.
   return findId;
-}
+};
 
 console.log(getSpeciesByIds('tigersId'));
 
@@ -24,7 +23,7 @@ function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   const verifySpecie = species.find((specie) => specie.name === animal);
   return verifySpecie.residents.every((ageId) => ageId.age >= age);
-}
+};
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
@@ -32,18 +31,19 @@ function getEmployeeByName(employeeName) {
   const employee = employees.find((name) => 
   name.firstName === employeeName || name.lastName === employeeName);
   return employee;
-}
+};
 // console.log(getEmployeeByName('Wishart'));
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
   return { ...associatedWith, ...personalInfo };
-}
+};
 
 function isManager(id) {
   // seu código aqui
-  return employees.some((employee) => employee.managers.find((idManager) => idManager === id));
-}
+  return employees.some((employee) => 
+  employee.managers.find((idManager) => idManager === id));
+};
 
 console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
@@ -51,7 +51,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   // seu código aqui
   const newEmployee = { id, firstName, lastName, managers, responsibleFor };
   return employees.push(newEmployee);
-}
+};
 
 function countAnimals(animal) {
 //   // seu código aqui
@@ -64,7 +64,18 @@ function countAnimals(animal) {
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  // const precos = Object.values(prices);
+  // const visits = Object.values(entrants);
+  // console.log(precos);
+  // console.log(visits);
+  // return (precos[0] * visits[0]) + (precos[1] * visits[1]) + (precos[2] * visits[2]);
+ const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+ const { Adult: adultPrice, Senior: seniorPrice, Child: childPrice } = prices;
+ return (Adult * adultPrice) + (Senior * seniorPrice) + (Child * childPrice);
 }
+
+// console.log(calculateEntry({ 'Adult': 2, 'Senior': 3, 'Child': 1 }));
 
 function getAnimalMap(options) {
   // seu código aqui
