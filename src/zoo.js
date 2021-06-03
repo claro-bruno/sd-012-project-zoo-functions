b6cc9f613157eee07ee0b7d66f10d33d0d64cf76
Willian Prado <willian_prado@hotmail.com>
2021-06-03 16:54:00 -0300

    Resolve os requisitos 7, 8 e 9

--- src/zoo.js
+++ src/zoo.js
@@ -13,32 +13,33 @@ const { species } = require('./data');
 const data = require('./data');
 
 const { employees } = data;
-// console.log(employees);
+const { prices } = data;
 
+// Req 1
 function getSpeciesByIds(...ids) {
   const mySpecies = [];
   ids.forEach((id) => mySpecies.push((species.find((specie) => specie.id === id))));
   return mySpecies;
 }
-
+// Req 2
 function getAnimalsOlderThan(animal, age) {
   const searchAnimal = species.find((specie) => specie.name === animal);
   const verifyAge = searchAnimal.residents.every((resident) => resident.age >= age);
   return verifyAge;
 }
-
+// Req 3
 function getEmployeeByName(employeeName) {
   let myEmployee = {};
   const test = employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
   myEmployee = { ...test };
   return myEmployee;
 }
-
+// Req 4
 function createEmployee(personalInfo, associatedWith) {
   const myEmployee = { ...personalInfo, ...associatedWith };
   return myEmployee;
 }
-
+// Req 5
 function isManager(id) {
   const manager = employees.reduce((acc, current) => {
     if (!acc.includes(...current.managers)) {
@@ -50,7 +51,7 @@ function isManager(id) {
   const checkManager = manager.includes(id);
   return checkManager;
 }
-
+// Req 6
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   const newPerson = {
     id,
@@ -60,31 +61,96 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
     responsibleFor };
   data.employees.push(newPerson);
 }
+// Req 7
+function countAnimals(speciesName) {
+  const animals = species.reduce((acc, current) => {
+    acc[current.name] = current.residents.length;
+    return acc;
+  }, {});
 
-function countAnimals(species) {
-  // seu código aqui
+  if (!speciesName) {
+    return animals;
+  }
+  return animals[speciesName];
 }
 
+// Req 8
 function calculateEntry(entrants) {
-  // seu código aqui
+  let price = 0;
+  if (entrants) {
+    const keys = Object.keys(entrants);
+    keys.forEach((key) => {
+      price += entrants[key] * prices[key];
+    });
+  }
+  return price;
+}
+// Auxiliam no req 9
+function generateAnimalObj() {
+  const animalMap = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [] };
+  return animalMap;
+}
+// Gera o mapeamento padrão das localizações das espécies
+function animalsDefault() {
+  const animalMap = generateAnimalObj();
+  species.forEach((specie) => {
+    animalMap[specie.location].push(specie.name);
+  });
+  return animalMap;
+}
+// Associa os nomes dos animais por espécie, com o sexo e ordenação como parametros
+function animalNames(sorted = false, sex = undefined) {
+  const animals = {};
+  species.forEach((specie) => {
+    let names = specie.residents.reduce((acc, current) => {
+      acc.push(current.name);
+      return acc;
+    }, []);
+    if ((sex === 'male') || (sex === 'female')) {
+      names = specie.residents.filter((each) => each.sex === sex).map((each) => each.name);
+    }
+    animals[specie.name] = names;
+    if (sorted) animals[specie.name] = names.sort();
+  });
+  return animals;
+}
+// Popula o objeto contendo as regiões do zoológico com os respectivos animais
+function animalMapping(sorted = false, sex = undefined) {
+  const animalMap = generateAnimalObj();
+  const animals = animalNames(sorted, sex);
+
+  species.forEach((specie) => {
+    animalMap[specie.location].push({ [specie.name]: animals[specie.name] });
+  });
+  return animalMap;
 }
 
+// Req 9
 function getAnimalMap(options) {
-  // seu código aqui
+  if ((!options) || (!options.includeNames)) {
+    return animalsDefault();
+  } if (options.includeNames) {
+    return animalMapping(options.sorted, options.sex);
+  }
 }
 
+// Req 10
 function getSchedule(dayName) {
   // seu código aqui
 }
-
+// Req 11
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
 }
-
+// Req 12
 function increasePrices(percentage) {
   // seu código aqui
 }
-
+// Req 13
 function getEmployeeCoverage(idOrName) {
   // seu código aqui
 }
