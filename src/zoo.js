3d5193656cac70e2794d963cf08bac37fd344f90
Gustavo-Mourao <gcmbh1@gmail.com>
2021-06-03 15:15:56 -0300

    Q1

--- src/zoo.js
+++ src/zoo.js
@@ -8,12 +8,26 @@ eslint no-unused-vars: [
   }
 ]
 */
-// Iniciando Projeto
+// 1. IMPLEMENTE A FUNÇÃO getSpeciesByIds
+// Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+
+// Observações técnicas
+
+// O parâmetro desta função pode ser alterado para atender ao requisito proposto
+// O que será avaliado
+
+// Caso receba nenhum parâmetro, necessário retornar um array vazio
+// Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id
+// Ao receber mais de um id, retorna um array com as espécies referentes aos ids
+
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.filter((arrayItem) => arrayItem.id === ids);
 }
+
+console.log(getSpeciesByIds('lionId'));
+// seu código aqui
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
