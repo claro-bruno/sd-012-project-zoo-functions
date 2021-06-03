6c6cf6e466f1153017736498d642c4b4c624bd1a
José Carlos <kalarruda@hotmail.com>
2021-06-03 11:45:13 -0300

    4-desafio

--- src/zoo.js
+++ src/zoo.js
@@ -12,10 +12,14 @@ eslint no-unused-vars: [
 const data = require('./data');
 const { employees } = require('./data');
 
+// 1=========================================================
+
 function getSpeciesByIds(...ids) {
   return data.species.filter((specie) => ids.includes(specie.id));
 }// se o filter não receber nenhum item ele retorna array vazio
 // console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
+
+// 2=========================================================
 
 function getAnimalsOlderThan(animal, age) {
   return data.species
@@ -24,6 +28,8 @@ function getAnimalsOlderThan(animal, age) {
 }
 // console.log(getAnimalsOlderThan('otters', 7));
 
+// 3=========================================================
+
 function getEmployeeByName(employeeName) {
   if (!employeeName) return {};
 
@@ -31,9 +37,31 @@ function getEmployeeByName(employeeName) {
 }
 // console.log(getEmployeeByName());
 
-// function createEmployee(personalInfo, associatedWith) {
+// 4=========================================================
 
-// }
+function createEmployee(personalInfo, associatedWith) {
+  const newEmployee = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor,
+  };
+  return newEmployee;
+}
+const info1 = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+const info2 = {
+  managers: ['c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'],
+  responsibleFor: ['0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'],
+};
+console.log(createEmployee(info1, info2));
+// 5=========================================================
 
 function isManager(id) {
   const employee = employees
@@ -41,13 +69,33 @@ function isManager(id) {
   // .some((isEmployee) => isEmployee.managers.length === 0 || isEmployee.managers.find((idManager) => idManager === id))// CEO
   return employee;
 }
+// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));l
 
-console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));// nigel
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));// burl
+// 6=========================================================
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  if (!newEmployee.managers) {
+    newEmployee.managers = [];
+  }
+  if (!newEmployee.responsibleFor) {
+    newEmployee.responsibleFor = [];
+  }
+  console.log(newEmployee);
+  employees.push(newEmployee);
+  return employees;
+}
+// addEmployee('39800c14-4b76-454a-858d-2f8d168146a7', 'John', 'Doe');
+// console.log(employees.length);
+
+// 7=========================================================
 
 // function countAnimals(species) {
 //   // seu código aqui
@@ -82,6 +130,8 @@ module.exports = {
   getAnimalsOlderThan,
   getEmployeeByName,
   isManager,
+  addEmployee,
+  createEmployee,
 };
 
 // module.exports = {
