7612b91c9618144721f6add6a23124b0bdefbeb5
Çuiz Henrique <luishenriqueee2010@hotmail.com>
2021-06-02 23:14:56 -0300

    Adiciona desafio 1 e 2

--- src/zoo.js
+++ src/zoo.js
@@ -12,57 +12,76 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  console.log(ids);
-  // entender o motivo de se usar .map antes de prosseguir
-  return ids.map((id) => data.species.find((animals) => id === animals.id));
+  return ids.map((idAnimal) => data.species.find((animals) => idAnimal === animals.id));
 }
+// console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 
 function getAnimalsOlderThan(animal, age) {
+  // Every, so tem que descobrir como :) OK
+  // Primeiro chegar ao nome da espécie OK
+  // Depois chegar ao nome dos animais Ok
+  // Depois chegar a idade dos animais  OK
+  // Verificação de idade OK
+  const animals = data.species.find((animalList) => animalList.name === animal);
+  const individuals = animals.residents;
+  const verify = individuals.every((idades) => idades.age > age);
+  return verify;
+}
+console.log(getAnimalsOlderThan('elephants', 10));
+
+function getEmployeeByName() {
   // seu código aqui
+  // employeeName
 }
 
-function getEmployeeByName(employeeName) {
+function createEmployee() {
   // seu código aqui
+  // personalInfo, associatedWith
 }
 
-function createEmployee(personalInfo, associatedWith) {
+function isManager() {
   // seu código aqui
+  // id
 }
 
-function isManager(id) {
+function addEmployee() {
   // seu código aqui
+  // id, firstName, lastName, managers, responsibleFor
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function countAnimals() {
   // seu código aqui
+  // species
 }
 
-function countAnimals(species) {
+function calculateEntry() {
   // seu código aqui
+  // entrants
 }
 
-function calculateEntry(entrants) {
+function getAnimalMap() {
   // seu código aqui
+  // options
 }
 
-function getAnimalMap(options) {
+function getSchedule() {
   // seu código aqui
+  // dayName
 }
 
-function getSchedule(dayName) {
+function getOldestFromFirstSpecies() {
   // seu código aqui
+  // id
 }
 
-function getOldestFromFirstSpecies(id) {
+function increasePrices() {
   // seu código aqui
+  // percentage
 }
 
-function increasePrices(percentage) {
+function getEmployeeCoverage() {
   // seu código aqui
-}
-
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
+  // idOrName
 }
 
 module.exports = {
