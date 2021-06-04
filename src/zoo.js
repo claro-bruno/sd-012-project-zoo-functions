ca6cc6b5227b7f1885fc7ab63eeba2fecf4d897a
Augusto <augustocm2002@yahoo.com.br>
2021-06-03 23:59:53 -0300

    Fazendo requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -23,73 +23,73 @@ console.log(getSpeciesByIds(''));
 
 // vamos começar!
 
-//1-Encontrando nome:
-
+// 1-Encontrando nome:
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   const accessSpecie = data.species.find((specie) => specie.name === animal);
-  //2 - Encontrando true ou false relacionado com a idade:
+  // 2 - Encontrando true ou false relacionado com a idade:
   return accessSpecie.residents.every((resident) => resident.age >= age);
 }
-console.log(getAnimalsOlderThan('lions', 3));
+console.log(getAnimalsOlderThan('lions'));
 
-
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(employeeName = {}) {
+  return data.employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName
+  === employeeName) || employeeName;
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function countAnimals(species) {
-  // seu código aqui
-}
+// function countAnimals(species) {
+//   // seu código aqui
+// }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+// function getSchedule(dayName) {
+//   // seu código aqui
+// }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function getEmployeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
-  calculateEntry,
-  getSchedule,
-  countAnimals,
-  getAnimalMap,
+  // calculateEntry,
+  // getSchedule,
+  // countAnimals,
+  // getAnimalMap,
   getSpeciesByIds,
   getEmployeeByName,
-  getEmployeeCoverage,
-  addEmployee,
-  isManager,
+  // getEmployeeCoverage,
+  // addEmployee,
+  // isManager,
   getAnimalsOlderThan,
-  getOldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+  // getOldestFromFirstSpecies,
+  // increasePrices,
+  // createEmployee,
 };
