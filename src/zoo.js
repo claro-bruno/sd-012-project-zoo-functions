35e4a63bfcfcaf8c496046dc5ccf40b1f68b2141
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-06-04 20:05:05 -0300

    terminado 5 requisito

--- src/zoo.js
+++ src/zoo.js
@@ -46,7 +46,13 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return (
+    employees.some((employee) => (
+      employee.managers.some((manager) => (
+        manager === id
+      ))
+    ))
+  );
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
