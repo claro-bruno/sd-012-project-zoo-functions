2a4079ab7f6d3c940717adaeef08bfd7024d4f0d
Camila Damásio <camiladamasioef@hotmail.com>
2021-06-04 14:35:02 -0300

    Requisito 1 faltando a terceira verificaçao; requisito 2 e abstraçao do requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -10,18 +10,43 @@ eslint no-unused-vars: [
 */
 
 
+const { species } = require('./data');
 const data = require('./data');
-
+// console.log(species);
 function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+  // O que será avaliado:
+  // console.log(ids)
+  // getSpeciesByIds(ids);
+  // Caso receba nenhum parâmetro, necessário retornar um array vazio;
+  if (!ids) { return [] }
 
+  // Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
+  if (ids.length = 1) { return species.filter((specie) => (specie.id === ids)); }
+
+  // Ao receber mais de um id, retorna um array com as espécies referentes aos ids
+  if (ids.length > 1) { return species.filter((specie, index) => (specie.id === ids[index])); }
+  // fazer um for para ele não mostrar apenas as informações do primeiro id
+
+
+}
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // O que será avaliado:
+
+  // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada (deve retornar um valor booleano);
+  return species.find((specie) => specie.name === animal).residents.every((resident) => resident.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // O que será avaliado:
+
+  // Sem parâmetros, retorna um objeto vazio;
+  if (!employeeName) { return [] }
+
+  // Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+  if (employeeName === employees.firstName) {  }
+
+  // Quando provido o último nome do funcionário, retorna o objeto do funcionário
+  if (employeeName === employees.lastName) { return  }
 }
 
 function createEmployee(personalInfo, associatedWith) {
