53dd5037b0189fc2bad89453304d21e82542c60e
Camila Damásio <camiladamasioef@hotmail.com>
2021-06-04 16:39:34 -0300

    Resolve erro lint e finaliza requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,98 +9,92 @@ eslint no-unused-vars: [
 ]
 */
 
-
 const { species } = require('./data');
 const data = require('./data');
 // console.log(species);
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // O que será avaliado:
-  // console.log(ids)
-  // getSpeciesByIds(ids);
   // Caso receba nenhum parâmetro, necessário retornar um array vazio;
-  if (!ids) { return [] }
-
   // Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
-  if (ids.length = 1) { return species.filter((specie) => (specie.id === ids)); }
-
-  // Ao receber mais de um id, retorna um array com as espécies referentes aos ids
-  if (ids.length > 1) { return species.filter((specie, index) => (specie.id === ids[index])); }
-  // fazer um for para ele não mostrar apenas as informações do primeiro id
-
-
+  // Ao receber mais de um id, retorna um array com as espécies referentes aos ids.
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
+  // Para solução da terceira verificação foi necessário fazer um for do ids com o map e dentro deste for, outro for de species com o find para verificar todas as species com cada id de ids.
+  // resolvido a terceira verificação com ajuda do colega Thalles Carneiro. Logo após foi feito o encurtamento do código.
 }
+
 function getAnimalsOlderThan(animal, age) {
   // O que será avaliado:
 
   // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada (deve retornar um valor booleano);
-  return species.find((specie) => specie.name === animal).residents.every((resident) => resident.age >= age);
+  return species.find((specie) => specie.name === animal)
+    .residents.every((resident) => resident.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
   // O que será avaliado:
 
-  // Sem parâmetros, retorna um objeto vazio;
-  if (!employeeName) { return [] }
+  // Sem parâmetros, retorna um objeto vazio
+  if (!employeeName) { return []; }
 
   // Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
-  if (employeeName === employees.firstName) {  }
+  // if (employeeName === data.employees.firstName) {  };
 
   // Quando provido o último nome do funcionário, retorna o objeto do funcionário
-  if (employeeName === employees.lastName) { return  }
+  // if (employeeName === employees.lastName) { return  };
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function countAnimals(species) {
-  // seu código aqui
-}
+// function countAnimals(species) {
+//   // seu código aqui
+// }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+// function calculateEntry(entrants) {
+//   // seu código aqui
+// }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+// function getAnimalMap(options) {
+//   // seu código aqui
+// }
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+// function getSchedule(dayName) {
+//   // seu código aqui
+// }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function getOldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function getEmployeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
-  calculateEntry,
-  getSchedule,
-  countAnimals,
-  getAnimalMap,
+  // calculateEntry,
+  // getSchedule,
+  // countAnimals,
+  // getAnimalMap,
   getSpeciesByIds,
   getEmployeeByName,
-  getEmployeeCoverage,
-  addEmployee,
-  isManager,
+  // getEmployeeCoverage,
+  // addEmployee,
+  // isManager,
   getAnimalsOlderThan,
-  getOldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+  // getOldestFromFirstSpecies,
+  // increasePrices,
+  // createEmployee,
 };
