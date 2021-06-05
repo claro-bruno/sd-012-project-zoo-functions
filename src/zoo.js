7dceacea25178c085a1cb2ebccb87849352109eb
Sarah <sarahgsb@hotmail.com>
2021-06-05 19:43:00 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -12,11 +12,12 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 // requisito 1
-function getSpeciesByIds() {
-  // ids
+function getSpeciesByIds(...ids) {
   // const speciesByIds = data.species.find((specie) => specie.id === ids);
-  // return speciesByIds;
+  const speciesByIds = data.species.filter((specie) => ids.find(id => specie.id === id));
+  return speciesByIds;
 }
+//console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 // requisito 2
 function getAnimalsOlderThan(animal, age) {
