e5c2ecfbeeb3a77a12ad490a27de04458bebbced
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-06-04 21:24:26 -0300

    Requisito 12

--- src/zoo.js
+++ src/zoo.js
@@ -105,6 +105,11 @@ function getOldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   // seu código aqui
+  const { prices } = data;
+  Object.keys(prices)
+    .forEach((key) => {
+      prices[key] = Math.round(prices[key] * (1 + (percentage / 100)) * 100) / 100;
+    });
 }
 
 function getEmployeeCoverage(idOrName) {
