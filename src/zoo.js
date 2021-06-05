111f07e544309f6ea3d2a157fb846a4902726424
lucasnahas <lucasahas@gmail.com>
2021-06-05 17:07:35 -0300

    salva o trabalho

--- src/zoo.js
+++ src/zoo.js
@@ -13,26 +13,26 @@ const { species, employees, hours, prices } = require('./data');
 
 function getSpeciesByIds(...ids) {
   return species.filter((value) => ids.includes(value.id));
-} // ok
+} // ok1
 
 function getAnimalsOlderThan(animal, age) {
   return species
     .find((value) => value.name === animal).residents.every((value) => value.age >= age);
-} // ok
+} // ok2
 
 function getEmployeeByName(employeeName) {
   return employees
     .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName)
     || {};
-} // ok
+} // ok3
 
 function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
-}
+} // ok4
 
 function isManager(id) {
   return employees.some(({ managers }) => managers.includes(id));
-} // ok
+} // ok5
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   const obj = {
@@ -43,7 +43,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
     responsibleFor,
   };
   employees.push(obj);
-} // ok
+} // ok6
 
 function countAnimals(specieName) {
   if (specieName !== undefined) {
@@ -53,7 +53,7 @@ function countAnimals(specieName) {
     acc[specie.name] = specie.residents.length;
     return acc;
   }, {});
-} // ok
+} // ok7
 
 function calculateEntry(entrants) {
   if (entrants === undefined || Object.keys(entrants).length === 0) {
@@ -61,11 +61,69 @@ function calculateEntry(entrants) {
   }
   return Object.keys(entrants)
     .reduce((acc, entrant) => acc + entrants[entrant] * prices[entrant], 0);
-} // ok
+} // ok8
 
-// function getAnimalMap(options) {
-//   // seu código aqui
-// }
+function getAnimalMap(options) {
+  const { includeNames, sex } = options;
+  const res = species.reduce((acc, specie) => {
+    const { name, location, residents } = specie;
+    if (acc[location] === undefined) {
+      acc[location] = [];
+    }    
+    acc[location][name] = [];
+    // let residents = acc[location][name].map((resident) => {
+    //   return resident.
+    // });
+
+
+
+    console.log('------------');
+    console.log(specie);
+    return acc
+  }, {});
+
+
+} getAnimalMap({});
+
+/*
+  1 Com species.reduce(OBJ), varrer todo o arr e ir acc o obj com as
+  localidades como chaves.
+  Se o local nao existir, criar a chave com uma arr vazia.
+  Se existir, acessar o valor e popular a arr com o animal 
+  Este resultado deve ser retornado caso nao haja argumentos na func.
+  {
+    NE: ['lions', 'giraffes'],
+    NW: ['tigers', 'bears', 'elephants'],
+    SE: ['penguins', 'otters'],
+    SW: ['frogs', 'snakes']
+  };
+  
+  2 Se includeNames: true, 
+  Olocal - A - Oanimal - Anomes
+  {
+    NE: [
+      { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] },
+      { giraffes: ['Gracia', 'Antone', 'Vicky', 'Clay', 'Arron', 'Bernard'] }
+    ]
+  }
+
+  3 Se sorted: true e se includeNames: true
+  {
+    NE: [
+      { lions: ['Dee', 'Faustino', 'Maxwell', 'Zena'] },
+      { giraffes: ['Antone', 'Arron', 'Bernard', 'Clay', 'Gracia', 'Vicky'] }
+    ]
+  }
+
+  4 sex: '*gender*' e se includeNames: true
+  5 sex: '*gender*' sorted: true e se includeNames: true
+  
+*/
+
+
+
+
+
 
 function getSchedule(dayName) {
   const obj = {};
@@ -80,7 +138,7 @@ function getSchedule(dayName) {
     return { [dayName]: obj[dayName] };
   }
   return obj;
-} // ok
+} // ok10
 
 function getOldestFromFirstSpecies(id) {
   const employee = employees.find((value) => value.id === id);
@@ -93,13 +151,13 @@ function getOldestFromFirstSpecies(id) {
     return acc;
   }, { age: 0 });
   return Object.values(oldest);
-} // ok
+} // ok11
 
 function increasePrices(percentage) {
   Object.keys(prices).forEach((key) => {
     prices[key] = Math.round(prices[key] * (1 + percentage / 100) * 100) / 100;
   });
-}
+} // ok12
 
 function anfi(idArr) {
   const namesArr = idArr.map((id) => (
@@ -126,13 +184,13 @@ function getEmployeeCoverage(idOrName) {
     species.find((specie) => specie.id === specieId).name));
   const name = `${resEmp.firstName} ${resEmp.lastName}`;
   return { [name]: animalsArr };
-}
+}// ok13
 
 module.exports = {
   calculateEntry,
   getSchedule,
   countAnimals,
-  // getAnimalMap,
+  getAnimalMap,
   getSpeciesByIds,
   getEmployeeByName,
   getEmployeeCoverage,
