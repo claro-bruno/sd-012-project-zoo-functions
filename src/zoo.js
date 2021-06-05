1ff034c4d3056942405169c5e56fd2ef105d603e
Sarah <sarahgsb@hotmail.com>
2021-06-05 19:46:05 -0300

    correcao lint requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -14,10 +14,10 @@ const data = require('./data');
 // requisito 1
 function getSpeciesByIds(...ids) {
   // const speciesByIds = data.species.find((specie) => specie.id === ids);
-  const speciesByIds = data.species.filter((specie) => ids.find(id => specie.id === id));
+  const speciesByIds = data.species.filter((specie) => ids.find((id) => specie.id === id));
   return speciesByIds;
 }
-//console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 // requisito 2
 function getAnimalsOlderThan(animal, age) {
