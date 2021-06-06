bdea3143f9833cee5cc869baaf6ac2dca9d23e27
Augusto <augustocm2002@yahoo.com.br>
2021-06-05 23:06:12 -0300

    Fazendo requisto 8 - Corrigindo eslint - alterando nome da const para d

--- src/zoo.js
+++ src/zoo.js
@@ -32,11 +32,18 @@ function getAnimalsOlderThan(animal, age) {
 // console.log(getAnimalsOlderThan('lions'));
 
 function getEmployeeByName(employeeName = {}) {
-  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName
-  === employeeName) || employeeName;
+  return (
+    data.employees.find(
+      (employee) => employee.firstName === employeeName
+        || employee.lastName === employeeName,
+    ) || employeeName
+  );
 }
 
-function createEmployee({ id, firstName, lastName }, { managers, responsibleFor }) {
+function createEmployee(
+  { id, firstName, lastName },
+  { managers, responsibleFor },
+) {
   return {
     id,
     firstName,
@@ -48,12 +55,18 @@ function createEmployee({ id, firstName, lastName }, { managers, responsibleFor 
 
 function isManager(id) {
   // eslint-disable-next-line max-len
-  const accessId = data.employees.some((employee) => employee.managers.some(((manage) => manage === id)));
-  return accessId;
+  const d = data.employees.some((employee) => employee.managers.some((manage) => manage === id));
+  return d;
 }
 // console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = [],
+) {
   return data.employees.push({
     id,
     firstName,
@@ -79,8 +92,11 @@ function countAnimals(species) {
 function calculateEntry(entrants) {
   if (entrants !== undefined) {
     const { Adult = 0, Child = 0, Senior = 0 } = entrants;
-    return data.prices.Adult * Adult
-     + data.prices.Child * Child + data.prices.Senior * Senior;
+    return (
+      data.prices.Adult * Adult
+      + data.prices.Child * Child
+      + data.prices.Senior * Senior
+    );
   }
 
   return 0;
