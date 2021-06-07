4d3b7fb5a8b8e22e426f6ba7070aa814139be261
igor <igorsife80@gmail.com>
2021-06-06 22:47:39 -0300

    resolve o requisto 5

--- src/zoo.js
+++ src/zoo.js
@@ -8,13 +8,9 @@ eslint no-unused-vars: [
   }
 ]
 */
-const {
-  species
-} = require('./data');
-const {
-  employees
-} = require('./data');
-const data = require('./data');
+const { species } = require('./data');
+const { employees } = require('./data');
+const { data } = require('./data');
 
 function getSpeciesByIds(...ids) {
   //return ids.map((idAnimal) => species.find((animals) => idAnimal === animals.id));
@@ -23,10 +19,8 @@ function getSpeciesByIds(...ids) {
       return id === specie.id;
     })
   })
-  console.log(getSpeciesByIds(ids))
 }
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 function getAnimalsOlderThan(animal, age) {
   return species
@@ -42,32 +36,17 @@ function getEmployeeByName(name) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  const newobject = {...personalInfo, ...associatedWith}
-  return newobject;
-  console.log(newobject)
-}
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992'
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
-  ]
-};
-console.log(createEmployee(personalInfo,associatedWith))
-/* function isManager(id) {
-  // seu código aqui
+  return {...personalInfo, ...associatedWith}
 }
 
+function isManager(id) {
+  return employees.some((employee) => {
+    return employee.managers.includes(id);
+  });
+}
+  //  https://www.alura.com.br/artigos/o-que-e-o-operador-ternario?gclid=Cj0KCQjw5PGFBhC2ARIsAIFIMNd7ReICV3cuH5g_2lyjEwAbJ1cO2XeOR2nLS7eNWKhyZg7VoIPJxEMaAk20EALw_wcB
+
+/*
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
@@ -109,7 +88,7 @@ module.exports = {
   getEmployeeByName,
   //  getEmployeeCoverage,
   //  addEmployee,
-  //  isManager,
+  isManager,
   getAnimalsOlderThan,
   //  getOldestFromFirstSpecies,
   //  increasePrices,
