d9f8c6e175bb272a8b6cdf38e923582ea2e0aa81
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-06-07 13:04:47 -0300

    resolucao req 4

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,10 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return {
+    ...personalInfo,
+    ...associatedWith
+  };
 }
 
 function isManager(id) {
