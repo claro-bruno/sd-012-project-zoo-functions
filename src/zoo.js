a5f75b7b38bb483b95ef1feebbd95ce6a7c9ecd9
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-06-07 15:24:09 -0300

    Resolve requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 //
 const data = require('./data');
 
-const { species, employees, prices } = data;
+const { species, employees, prices, hours } = data;
 
 function getSpeciesByIds(...ids) {
   return species.filter((specie, index) => specie.id === ids[index]);
@@ -68,8 +68,21 @@ function getAnimalMap() {
   // seu código aqui
 }
 
-function getSchedule() {
-  // seu código aqui
+function getSchedule(dayName) {
+  const diasUteis = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+
+  if (dayName === undefined) {
+    return diasUteis;
+  }
+  return { [dayName]: diasUteis[dayName] };
 }
 
 function getOldestFromFirstSpecies() {
