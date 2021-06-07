61bfa69591d48bc1ca987a3c9bc3c3dd57e5dd88
igor <igorsife80@gmail.com>
2021-06-06 23:09:51 -0300

    refatora slint

--- src/zoo.js
+++ src/zoo.js
@@ -10,23 +10,23 @@ eslint no-unused-vars: [
 */
 const { species } = require('./data');
 const { employees } = require('./data');
-const { data } = require('./data');
+// const { data } = require('./data');
 
 function getSpeciesByIds(...ids) {
-  //return ids.map((idAnimal) => species.find((animals) => idAnimal === animals.id));
-  return ids.map((id)=> {
+// return ids.map((idAnimal) => species.find((animals) => idAnimal === animals.id));
+  return ids.map((id) => {
     return species.find((specie) => {
       return id === specie.id;
-    })
-  })
+    });
+  });
 }
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
 function getAnimalsOlderThan(animal, age) {
   return species
-    .find(({
-      name
-    }) => name === animal).residents.every((animalonly) => animalonly.age >= age);
+    .find(({ name }) =>
+      name === animal).residents.every((animalonly) =>
+      animalonly.age >= age);
 }
 // referencia  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/every
 
@@ -36,7 +36,7 @@ function getEmployeeByName(name) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith}
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
@@ -44,7 +44,7 @@ function isManager(id) {
     return employee.managers.includes(id);
   });
 }
-  //  https://www.alura.com.br/artigos/o-que-e-o-operador-ternario?gclid=Cj0KCQjw5PGFBhC2ARIsAIFIMNd7ReICV3cuH5g_2lyjEwAbJ1cO2XeOR2nLS7eNWKhyZg7VoIPJxEMaAk20EALw_wcB
+//  https://www.alura.com.br/artigos/o-que-e-o-operador-ternario?gclid=Cj0KCQjw5PGFBhC2ARIsAIFIMNd7ReICV3cuH5g_2lyjEwAbJ1cO2XeOR2nLS7eNWKhyZg7VoIPJxEMaAk20EALw_wcB
 
 /*
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
