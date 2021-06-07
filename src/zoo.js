ee24bb41b4fe4cff2269d7598b9f3b185a24e45b
igor <igorsife80@gmail.com>
2021-06-07 10:45:29 -0300

    refatora slint novamente

--- src/zoo.js
+++ src/zoo.js
@@ -14,19 +14,13 @@ const { employees } = require('./data');
 
 function getSpeciesByIds(...ids) {
 // return ids.map((idAnimal) => species.find((animals) => idAnimal === animals.id));
-  return ids.map((id) => {
-    return species.find((specie) => {
-      return id === specie.id;
-    });
-  });
+  return ids.map((id) => species.find((specie) => id === specie.id));
 }
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
 function getAnimalsOlderThan(animal, age) {
   return species
-    .find(({ name }) =>
-      name === animal).residents.every((animalonly) =>
-      animalonly.age >= age);
+    .find(({ name }) => name === animal).residents.every((animalonly) => animalonly.age >= age);
 }
 // referencia  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/every
 
@@ -40,9 +34,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return employees.some((employee) => {
-    return employee.managers.includes(id);
-  });
+  return employees.some((employee) => employee.managers.includes(id));
 }
 //  https://www.alura.com.br/artigos/o-que-e-o-operador-ternario?gclid=Cj0KCQjw5PGFBhC2ARIsAIFIMNd7ReICV3cuH5g_2lyjEwAbJ1cO2XeOR2nLS7eNWKhyZg7VoIPJxEMaAk20EALw_wcB
 
