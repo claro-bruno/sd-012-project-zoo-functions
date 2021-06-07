b9869561bc006b4dab7e6864f9c768928873a598
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-06-07 13:01:57 -0300

    resolucao req 3

--- src/zoo.js
+++ src/zoo.js
@@ -21,7 +21,10 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if( !employeeName ){
+    return {};
+  }
+  return employees.find(((employee) => employee.firstName === employeeName || employee.lastName === employeeName));
 }
 
 function createEmployee(personalInfo, associatedWith) {
