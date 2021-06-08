34797917f95bd5ab588648bc39da880fc70f1eb5
Henrique Remedios <henriqueremedios@gmail.com>
2021-06-07 23:24:57 -0300

    refatora

--- src/zoo.js
+++ src/zoo.js
@@ -18,51 +18,51 @@ function getSpeciesByIds(...ids) {
   return allAnimals;
 }
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui 
+function getAnimalsOlderThan() {
+  // seu código aqui (animal, age)
 }
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui 
+function getEmployeeByName() {
+  // seu código aqui (employeeName)
 }
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui 
-}
-
-function isManager(id) {
-  // seu código aqui 
+function createEmployee() {
+  // seu código aqui (personalInfo, associatedWith)
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui 
+function isManager() {
+  // seu código aqui  
 }
 
-function countAnimals(species) {
-  // seu código aqui 
+function addEmployee() {
+  // seu código aqui (id, firstName, lastName, managers, responsibleFor)
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui 
+function countAnimals(){
+  // seu código aqui (species) 
 }
 
-function getAnimalMap(options) {
-  // seu código aqui 
+function calculateEntry() {
+  // seu código aqui (entrants)
 }
 
-function getSchedule(dayName) {
-  // seu código aqui 
+function getAnimalMap() {
+  // seu código aqui (options)
 }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getSchedule() {
+  // seu código aqui (dayName)
 }
 
-function increasePrices(percentage) {
-  // seu código aqui 
+function getOldestFromFirstSpecies() {
+  // seu código aqui (id)
 }
 
-function getEmployeeCoverage(idOrName) {
-  // seu código aqui 
+function increasePrices() {
+  // seu código aqui (percentage)
+}
+
+function getEmployeeCoverage() {
+  // seu código aqui (idOrName)
 }
 
 module.exports = {
