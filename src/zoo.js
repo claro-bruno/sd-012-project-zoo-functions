48e85edafe63c91deeaf2b3c8929b5b375b99fdf
Wadson <wadson.dev92@gmail.com>
2021-06-07 22:18:23 -0300

    requisito 4, linter ok

--- src/zoo.js
+++ src/zoo.js
@@ -33,7 +33,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith};
+  return { ...personalInfo, ...associatedWith };
 }
 
 // function isManager(id) {
