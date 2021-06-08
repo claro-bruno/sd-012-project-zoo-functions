1de35bb89464c457850fd7b623e12ef5381335d2
LuctReis <luctreis@gmail.com>
2021-06-08 18:56:59 -0300

    Requisito 4 Concluido

--- src/zoo.js
+++ src/zoo.js
@@ -30,7 +30,9 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmployee = {};
+  Object.assign(newEmployee, personalInfo, associatedWith);
+  return newEmployee;
 }
 
 function isManager(id) {
