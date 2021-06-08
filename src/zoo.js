302933bc9e6ba75f4fb332beb233a22f0da257cb
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-06-08 11:38:21 -0300

    questao 10, corrigindo

--- src/zoo.js
+++ src/zoo.js
@@ -78,11 +78,28 @@ const test = {
       SW: []
     };
 }
+*/
 
 function getSchedule(dayName) {
 // Questao 10
+
+  const weeks = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+
+  if (!dayName) {
+    return weeks;
+  }
+  const weekSchedule = { [dayName]: weeks[dayName] };
+  return weekSchedule;
 }
-*/
+
 /*
 function getOldestFromFirstSpecies(id) {
 // Questao 11
@@ -109,7 +126,7 @@ function getEmployeeCoverage(idOrName) {
 
 module.exports = {
   calculateEntry,
-  //  getSchedule,
+  getSchedule,
   countAnimals,
   //  getAnimalMap,
   getSpeciesByIds,
