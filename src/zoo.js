a2a520cab72ef4363cc4a6f85c971fee16840e13
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-06-08 14:09:30 -0300

    adiciona requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -82,9 +82,19 @@ const calculateEntry = (entrants) => {
 //   // seu código aqui
 // }
 
-// function getSchedule(dayName) {
-//   // seu código aqui
-// }
+function getSchedule(dayName) {
+  const days = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  if (!dayName) return days;
+  return { [dayName]: days[dayName] };
+}
 
 // function getOldestFromFirstSpecies(id) {
 //   // seu código aqui
@@ -100,7 +110,7 @@ const calculateEntry = (entrants) => {
 
 module.exports = {
   calculateEntry,
-  // getSchedule,
+  getSchedule,
   countAnimals,
   // getAnimalMap,
   getSpeciesByIds,
