0ba231de7b17ccfbfd35677f9793d266b6f73c12
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-09 11:07:45 -0300

    correçoes do lint

--- src/zoo.js
+++ src/zoo.js
@@ -24,7 +24,7 @@ function getAnimalsOlderThan(animal, age) {
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
-  } if else {
+  } else {
     return data.employees.find((intemArray) => {
       intemArray.firstName === employeeName || intemArray.lastName === employeeName
     });
