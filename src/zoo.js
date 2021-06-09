6789bb8280c0496082472e4bdf436c4d8bf1a464
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-09 11:05:50 -0300

    correçoes do lint

--- src/zoo.js
+++ src/zoo.js
@@ -12,39 +12,40 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.filter((specie, index) => specie.id === ids[index]); 
+  return data.species.filter((specie, index) => specie.id === ids[index]);
 }
 
 function getAnimalsOlderThan(animal, age) {
   return data.species.some((intemArray) => {
-    intemArray.name === animal && intemArray.residents.every((item) => item.age >= age )
+    intemArray.name === animal && intemArray.residents.every((item) => item.age >= age);
   });
 }
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
-  }
-  else {
+  } if else {
     return data.employees.find((intemArray) => {
       intemArray.firstName === employeeName || intemArray.lastName === employeeName
     });
-  }
+  };
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return personalInfo, associatedWith;
+  return (personalInfo, associatedWith);
 }
 
 function isManager(id) {
-  return data.employees.some((itemArray) => itemArray.id === id && (itemArray.managers).length <= 1);
+  return data.employees.some((itemArray) => {
+    itemArray.id === id && (itemArray.managers).length <= 1
+  });
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  return data.employees.push({id: id, firstName: firstName, lastName: lastName, managers: managers, responsibleFor: responsibleFor
-  });
+  return (id, firstName, lastName, managers, responsibleFor
+  );
 }
-//addEmployee(2, 'tarcio', 'moura', ['tau'], [5])
+
 function countAnimals(species) {
   return species;
 }
