a011867408322529911d83ac6495acb11d54749a
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-06-09 12:10:21 -0300

    correcao lint

--- src/test.js
+++
@@ -1,7 +0,0 @@
-
-function batata (param) {
-    if (param !== undefined) console.log(param);
-    if (param['kao']) console.log(param['kao'])
-}
-
-batata({kao: 4, batat: 5})
--- src/zoo.js
+++ src/zoo.js
@@ -17,12 +17,16 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  return data.species.some((specie) => specie.name === animal && specie.residents.every((resident) => resident.age >= age));
+  return data.species.some((specie) => {
+    return specie.name === animal && specie.residents.every((resident) => resident.age >= age)
+  });
 }
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) return {};
-  return data.employees.find((employee) => (employee.firstName === employeeName || employee.lastName === employeeName));
+  return data.employees.find((employee) => {
+    return (employee.firstName === employeeName || employee.lastName === employeeName)
+  });
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -40,52 +44,62 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function countAnimals(species) {
-  if (species !== undefined) return data.species.find((specie) => specie.name === species).residents.length
-  let obj = {};
-  data.species.forEach((specie) => obj[specie.name] = Object.keys(specie.residents).length);
+  if (species !== undefined) {
+    return data.species.find((specie) => specie.name === species).residents.length;
+  }
+  const obj = {};
+  data.species.forEach((specie) => {
+    obj[specie.name] = Object.keys(specie.residents).length;
+  })
   return obj;
 }
 
 function calculateEntry(entrants) {
-  if (entrants === undefined || entrants === {}) return 0;
+  if (entrants === undefined) return 0;
   let total = 0;
-  if (entrants['Adult']) total += entrants['Adult'] * data.prices['Adult'];
-  if (entrants['Senior']) total += entrants['Senior'] * data.prices['Senior'];
-  if (entrants['Child']) total += entrants['Child'] * data.prices['Child'];
+  if (entrants.Adult) total += entrants.Adult * data.prices.Adult;
+  if (entrants.Senior) total += entrants.Senior * data.prices.Senior;
+  if (entrants.Child) total += entrants.Child * data.prices.Child;
   return total;
 }
 
 function getAnimalMap(options) {
+  return options;
 }
 
 function getSchedule(dayName) {
-  schedule = {
-    'Tuesday': `Open from ${data.hours.Tuesday.open}am until ${data.hours.Tuesday.close - 12}pm`,
-    'Wednesday': `Open from ${data.hours.Wednesday.open}am until ${data.hours.Wednesday.close - 12}pm`,
-    'Thursday': `Open from ${data.hours.Thursday.open}am until ${data.hours.Thursday.close - 12}pm`,
-    'Friday': `Open from ${data.hours.Friday.open}am until ${data.hours.Friday.close - 12}pm`,
-    'Saturday': `Open from ${data.hours.Saturday.open}am until ${data.hours.Saturday.close - 12}pm`,
-    'Sunday': `Open from ${data.hours.Sunday.open}am until ${data.hours.Sunday.close - 12}pm`,
-    'Monday': 'CLOSED'
+  const schedule = {
+    Tuesday: `Open from ${data.hours.Tuesday.open}am until ${data.hours.Tuesday.close - 12}pm`,
+    Wednesday: `Open from ${data.hours.Wednesday.open}am until ${data.hours.Wednesday.close - 12}pm`,
+    Thursday: `Open from ${data.hours.Thursday.open}am until ${data.hours.Thursday.close - 12}pm`,
+    Friday: `Open from ${data.hours.Friday.open}am until ${data.hours.Friday.close - 12}pm`,
+    Saturday: `Open from ${data.hours.Saturday.open}am until ${data.hours.Saturday.close - 12}pm`,
+    Sunday: `Open from ${data.hours.Sunday.open}am until ${data.hours.Sunday.close - 12}pm`,
+    Monday: 'CLOSED',
   };
   if (dayName === undefined) return schedule;
-  let day = {};
+  const day = {};
   day[dayName] = schedule[dayName];
   return day;
 }
 
 function getOldestFromFirstSpecies(id) {
-  return Object.values(data.species.filter((specie) => specie.id === data.employees.find((employee) => employee.id === id).responsibleFor[0])[0].residents.sort((a, b) => b.age - a.age)[0]);;
+  const filterId = data.species.filter((specie) => {
+    return specie.id === data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  });
+  return Object.values(filterId[0].residents.sort((a, b) => b.age - a.age)[0]);
 }
 
 function increasePrices(percentage) {
-  data.prices['Adult'] = Math.round((data.prices['Adult'] + data.prices['Adult'] * (percentage / 100) + Number.EPSILON) * 100) / 100;
-  data.prices['Senior'] = Math.round((data.prices['Senior'] + data.prices['Senior'] * (percentage / 100) + Number.EPSILON) * 100) / 100;
-  data.prices['Child'] = Math.round((data.prices['Child'] + data.prices['Child'] * (percentage / 100) + Number.EPSILON) * 100) / 100;
+  const aux = (percentage / 100) + Number.EPSILON;
+  data.prices.Adult = Math.round((data.prices.Adult + data.prices.Adult * aux) * 100) / 100;
+  data.prices.Senior = Math.round((data.prices.Senior + data.prices.Senior * aux) * 100) / 100;
+  data.prices.Child = Math.round((data.prices.Child + data.prices.Child * aux) * 100) / 100;
   return data.prices;
 }
 
 function getEmployeeCoverage(idOrName) {
+  return idOrName;
 }
 
 module.exports = {
