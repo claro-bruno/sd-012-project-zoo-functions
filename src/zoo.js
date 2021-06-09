d2fc05b0d88bcfdcfb6238bce33f836a8dc3e2e3
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-06-09 12:31:17 -0300

    lint

--- src/zoo.js
+++ src/zoo.js
@@ -66,9 +66,10 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
+  const serio = data.hours.Wednesday.open;
   const schedule = {
     Tuesday: `Open from ${data.hours.Tuesday.open}am until ${data.hours.Tuesday.close - 12}pm`,
-    Wednesday: `Open from ${data.hours.Wednesday.open}am until ${data.hours.Wednesday.close-12}pm`,
+    Wednesday: `Open from ${serio}am until ${data.hours.Wednesday.close - 12}pm`,
     Thursday: `Open from ${data.hours.Thursday.open}am until ${data.hours.Thursday.close - 12}pm`,
     Friday: `Open from ${data.hours.Friday.open}am until ${data.hours.Friday.close - 12}pm`,
     Saturday: `Open from ${data.hours.Saturday.open}am until ${data.hours.Saturday.close - 12}pm`,
