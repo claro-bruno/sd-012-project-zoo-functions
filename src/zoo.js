d74a64e194e8a3838b0cbf04681c8d2da868378b
TarcisioMoura7 <bigtharcisiomoura@gmail.com>
2021-06-09 20:55:27 -0300

    correçao lint

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { prices, species } = require('./data');
+// const { prices, species } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
