00b1fe0117faf7d633516932c532ffc6e514c11e
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-09 16:24:28 -0300

    correçao lint

--- src/zoo.js
+++ src/zoo.js
@@ -69,19 +69,15 @@ function countAnimals(species) {
 function calculateEntry(entrants) {
   if (entrants === undefined || Object.keys(entrants).length === 0) {
     return 0;
-  }
+  };
   
   let Adult = entrants.Adult;
   if (Adult === undefined) Adult = 0;
   let Child = entrants.Child;
-  if (Child === undefined) {
-      Child = 0;
-  }
-  let Senior = entrants.Senior
+  if (Child === undefined) Child = 0;
+  let Senior = entrants.Senior;
   if (Senior === undefined) Senior = 0;
   return Child * prices.Child + Senior * prices.Senior + Adult * prices.Adult;
-  
-  //console.log(typeof entrants.Child)
 }
 
 function getAnimalMap(options) {
