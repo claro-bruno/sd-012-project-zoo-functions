11a62fe6fe92cefb86a927e45760784573116e6a
Sarah <sarahgsb@hotmail.com>
2021-06-11 09:46:09 -0300

    requisito 7

--- src/zoo.js
+++ src/zoo.js
@@ -64,14 +64,32 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 // console.log(addEmployee('39800c14-4b76-454a-858d-2f8d168146a7', 'John', 'Doe', managers = [], responsibleFor = []));
 
-// requisito 7
-function countAnimals() {
-  // seu código aqui species
+// REQUISITO 7
+// Consultei o respositório de Vitor Hugo Braz leite para fazer essa parte.
+// Link: https://github.com/tryber/sd-011-project-zoo-functions/pull/162
+
+// Explicação:
+// Para cada especie, acessa o nome do animal e retorna a quantidade em um objeto.
+// Sem parametros retorna animais e suas quantidades "empurrando" para dentro do objeto com auxilio de object destructuring.
+// Com o nome de uma espécie de animal, retorna somente a quantidade na linha 82.
+function countAnimals(species) {
+  let obj = {};
+  if (species === undefined) {
+    data.species.forEach((specie) => {
+      obj[`${specie.name}`] = specie.residents.length;
+    });
+  } else {
+    obj = data.species.find((specie) => specie.name === species).residents.length;
+  }
+  return obj;
 }
+
+// console.log(countAnimals());
 
 // requisito 8
 function calculateEntry() {
-  // seu código aqui entrants
+
+  // // seu código aqui entrants
 }
 
 // requisito 9
@@ -81,18 +99,26 @@ function getAnimalMap() {
 
 // requisiito 10
 function getSchedule() {
-  // seu código aqui dayName
+
 }
+// seu código aqui dayName
+
+// console.log(getSchedule('Tuesday'));
 
 // requisito 11
 function getOldestFromFirstSpecies() {
-  // seu código aqui id
-}
+  // - Passado o id de um funcionário, encontra a primeira espécie de animal
+  // const employeesID = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  // const animals = data.species.find((specie) => specie.id === employeesID).residents;
+  // const getOldest = animals.reduce;
 
+}
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 // requisito 12
 function increasePrices() {
-  // seu código aqui percentage
+
 }
+// console.log(increasePrices(20));
 
 // requisito 13
 function getEmployeeCoverage() {
