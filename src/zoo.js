81acabbc5eeaf457bc1775f06bd52d47a9191f88
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-06-15 14:05:35 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,14 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return ids.map((pegarId) => data.species.find((currentAnimalId) => currentAnimalId.id === pegarId));
 }
+// console.log(getSpeciesByIds())
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
+
+// console.log(data.species)
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
