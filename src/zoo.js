5f88b2b7c65a748b002722f98dcbae2721a71803
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-06-15 14:10:28 -0300

    requisito 1 lint

--- src/zoo.js
+++ src/zoo.js
@@ -1,24 +1,19 @@
-/*
-eslint no-unused-vars: [
-  "error",
-  {
-    "args": "none",
-    "vars": "local",
-    "varsIgnorePattern": "data"
-  }
-]
-*/
+
+// eslint no-unused-vars: [
+//   "error",
+//   {
+//     "args": "none",
+//     "vars": "local",
+//     "varsIgnorePattern": "data"
+//   }
+// ]
+
 
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  // seu código aqui
   return ids.map((pegarId) => data.species.find((currentAnimalId) => currentAnimalId.id === pegarId));
 }
-// console.log(getSpeciesByIds())
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
-
-// console.log(data.species)
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
