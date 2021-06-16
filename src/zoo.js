649b07020d0a3fcd09a40bd3f6b9c3cad9a0f0b4
AndZoni <lorenzoniandre@outlook.com>
2021-06-16 16:19:23 +0100

    Add Requirement 8

--- src/zoo.js
+++ src/zoo.js
@@ -59,9 +59,14 @@ function countAnimals(speciesName) {
   }
   return allAnimals[speciesName];
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+  function calculateEntry(entrants) {
+    if (entrants === undefined || Object.keys(entrants).length === 0) {
+      return 0;
+    }
+    const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+    return Adult * data.prices.Adult + Senior * data.prices.Senior + Child * data.prices.Child;
+  }
+  
 
 function getAnimalMap(options) {
   // seu código aqui
