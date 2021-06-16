019a7b096707e1b15091dde2347b478209af8c88
AndZoni <lorenzoniandre@outlook.com>
2021-06-16 15:28:14 +0100

    Add Requiriment 4

--- src/zoo.js
+++ src/zoo.js
@@ -34,7 +34,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
