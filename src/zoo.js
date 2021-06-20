aa7c8489b2f99a5eb6ff8031b39857c1f6ace6f4
Ana Laura <analauraalves25@gmail.com>
2021-06-19 22:37:10 -0300

    quinto requisito ok

--- src/zoo.js
+++ src/zoo.js
@@ -52,7 +52,11 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
+  const find = employees.some(({ managers }) => managers.includes(id));
+  return find;
 }
+
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
