89c7991b93e0f9ebb2137b0a4a6674cb92a8e5af
Ana Laura <analauraalves25@gmail.com>
2021-06-19 22:38:00 -0300

    retira console.log da funçao isManager

--- src/zoo.js
+++ src/zoo.js
@@ -56,8 +56,6 @@ function isManager(id) {
   return find;
 }
 
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
-
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
   const employeeAdd = employees.push({ id, firstName, lastName, managers, responsibleFor });
