5049c6015a039b0f451eb0fe7fd8e768ca3d22ff
Aldria Melo <aldriamelo.academico@gmail.com>
2021-06-22 01:25:12 -0300

    adiciona requesito 2

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 
 const { species, employees } = require('./data');
-const data = require('./data');
+// const data = require('./data');
 
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) return [];
